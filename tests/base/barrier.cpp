8dae1983000153d0166027a3c0d772898dc62fc1
Stefan Eilemann <eilemann@gmail.com>
2005-07-04 07:32:34 +0000

    some missing files

---
+++ tests/base/barrier.cpp
@@ -0,0 +1,51 @@
+
+#include <eq/base/barrier.h>
+#include <eq/base/thread.h>
+#include <iostream>
+
+using namespace eqBase;
+using namespace std;
+
+#define MAXTHREADS 256
+
+volatile size_t nThreads;
+Barrier*        barrier;
+
+class Test : public Thread
+{
+public:
+    Test() : Thread( Thread::PTHREAD ) {}
+    virtual int run()
+        {
+            const size_t num = barrier->enter( nThreads );
+            cerr << " " << num;
+            return EXIT_SUCCESS;
+        }
+};
+
+int main( int argc, char **argv )
+{
+    barrier = new Barrier( Thread::PTHREAD );
+    Test threads[MAXTHREADS];
+
+    for( nThreads = MAXTHREADS; nThreads>1; nThreads = nThreads>>1 )
+    {
+        cerr << endl << nThreads << " threads";
+
+        for( size_t i=0; i<nThreads; i++ )
+            threads[i].start();
+
+        for( size_t i=0; i<nThreads; i++ )
+        {
+            if( !threads[i].join( ))
+            {
+                cerr << "Could not join thread " << i << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    delete barrier;
+    return EXIT_SUCCESS;
+}
+
---
+++ tests/base/lock.cpp
@@ -0,0 +1,54 @@
+
+#include <eq/base/lock.h>
+#include <eq/base/thread.h>
+#include <iostream>
+
+using namespace eqBase;
+using namespace std;
+
+#define MAXTHREADS 256
+
+volatile size_t nThreads;
+Lock*           lock;
+
+class Test : public Thread
+{
+public:
+    Test() : Thread( Thread::PTHREAD ) {}
+    virtual int run()
+        {
+            lock->set();
+            lock->unset();
+            return EXIT_SUCCESS;
+        }
+};
+
+int main( int argc, char **argv )
+{
+    lock = new Lock( Thread::PTHREAD );
+    lock->set();
+
+    Test threads[MAXTHREADS];
+    for( nThreads = MAXTHREADS; nThreads>1; nThreads = nThreads>>1 )
+    {
+        cerr << nThreads << " threads" << endl;
+        for( size_t i=0; i<nThreads; i++ )
+            threads[i].start();
+
+        lock->unset();
+
+        for( size_t i=0; i<nThreads; i++ )
+        {
+            if( !threads[i].join( ))
+            {
+                cerr << "Could not join thread " << i << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        lock->set();
+    }
+
+    delete lock;
+    return EXIT_SUCCESS;
+}
+
