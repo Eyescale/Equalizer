42033def20ea81c311248f5bb21badce89422a23
Stefan Eilemann <eilemann@gmail.com>
2006-03-13 09:14:36 +0000

    missing file

---
+++ examples/flip/node.h
@@ -0,0 +1,19 @@
+
+/* Copyright (c) 2006, Stefan Eilemann <eile@equalizergraphics.com> 
+   All rights reserved. */
+
+#ifndef EQ_FLIP_NODE_H
+#define EQ_FLIP_NODE_H
+
+#include <eq/eq.h>
+
+#include "frameData.h"
+
+class Node : public eq::Node
+{
+protected:
+    bool init( const uint32_t initID )
+        { return true; }
+};
+
+#endif // EQ_FLIP_NODE_H
