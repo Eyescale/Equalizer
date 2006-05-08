9d2ab78bf25f5c527ec002788e2223de2b44e397
Stefan Eilemann <eilemann@gmail.com>
2006-05-08 09:20:03 +0000

    Linux x86_64 build fixes

--- make/Linux.x86_64.mk
+++ make/Linux.x86_64.mk
@@ -4,9 +4,6 @@ VARIANTS    = 32 64
 ifdef VARIANT
   CXXFLAGS    += -m$(VARIANT) -fPIC
   DSO_LDFLAGS += -m$(VARIANT)
-else
-  CXXFLAGS    += -m$(VARIANT1) -fPIC
-  DSO_LDFLAGS += -m$(VARIANT1)
 endif
 
 DSO_LDFLAGS        += -shared -luuid
@@ -14,13 +11,10 @@ DSO_SUFFIX          = so
 WINDOW_SYSTEM      += GLX
 
 ifeq ($(findstring 32, $(VARIANT)),32)
-  LDFLAGS          += -melf_i386
-endif
-
-ifeq ($(findstring 32, $(VARIANT1)),32)
+  LDFLAGS            += -m32
   WINDOW_SYSTEM_LIBS += -L/usr/X11R6/lib -lX11 -lGL
 else
-  WINDOW_SYSTEM_LIBS += -L/usr/X11R6/lib$(VARIANT1) -lX11 -lGL
+  WINDOW_SYSTEM_LIBS += -L/usr/X11R6/lib64 -lX11 -lGL
 endif
 
 WINDOW_SYSTEM_INCS += -I/usr/X11R6/include
