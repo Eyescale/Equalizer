0e5f01a9a62ebd33d60174594030d9afa7995704
Stefan Eilemann <eilemann@gmail.com>
2006-07-09 14:04:37 +0000

    Tweak build script

--- tools/cf_build.sh
+++ tools/cf_build.sh
@@ -4,9 +4,10 @@
 # machines. Note the hardcoded build directory below.
 # Usage : <name> <email>
 
+PATH=${PATH}:/sw/bin
 cd $HOME/Software/build/equalizer/trunk/src
 exec 1>`hostname`.make
 exec 2>&1
 svn up
-make clean || mail -s "Equalizer build failure (make clean) on `hostname`" $1 < `hostname`.make
-make || mail -s "Equalizer build failure (make) on `hostname`" $1 < `hostname`.make
+make clean || mail -s "Equalizer build failure [make clean] on `hostname`" $1 < `hostname`.make
+make || mail -s "Equalizer build failure [make] on `hostname`" $1 < `hostname`.make
