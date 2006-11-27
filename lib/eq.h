35372bd9bc76d100c8e8f40e1426326c0870f635
Stefan Eilemann <eilemann@gmail.com>
2006-11-27 18:06:13 +0000

    Fix build

--- lib/eq.h
+++ lib/eq.h
@@ -10,7 +10,6 @@
 #include <eq/client/configParams.h>
 #include <eq/client/event.h>
 #include <eq/client/frame.h>
-#include <eq/client/frustum.h>
 #include <eq/client/global.h>
 #include <eq/client/init.h>
 #include <eq/client/log.h>
