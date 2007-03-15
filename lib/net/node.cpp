84727c67c16329c4956443e392c58bfaefee5f88
Stefan Eilemann <eilemann@gmail.com>
2007-03-15 09:26:10 +0000

    [ ] May break build [ ] Breaks existing applications [ ] Bugfix [ ] New Feature [x] Cleanup [ ] Optimization [ ] Documentation
    Description: Reenable swap barrier in hactar.eqc

--- lib/net/node.cpp
+++ lib/net/node.cpp
@@ -1354,7 +1354,7 @@ string Node::_createRemoteCommand( RefPtr<Node> node )
         !( program[0] == '/' || program[0] == '\\' ))
         // !full path without drive letter
 
-        program = _workDir + '/' + program;
+        program = _workDir + '/' + program; // add _workDir to relative path
 #else
     if( program[0] != '/' )
         program = _workDir + '/' + program;
