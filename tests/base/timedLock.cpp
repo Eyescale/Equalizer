e16dee3ab49cc478242bf4cf044077705f0c1b76
Stefan Eilemann <eilemann@gmail.com>
2007-03-16 08:32:20 +0000

    [ ] May break build [ ] Breaks existing applications [ ] Bugfix [ ] New Feature [x] Cleanup [ ] Optimization [ ] Documentation
    Description: Disable warning

--- tests/base/timedLock.cpp
+++ tests/base/timedLock.cpp
@@ -16,14 +16,14 @@ int main( int argc, char **argv )
     TEST( lock.set( ));
 
     Clock clock;
-    TEST( !lock.set( 1000.0f ));
+    TEST( !lock.set( 1000 ));
     float time = clock.getTimef();
 
     TESTINFO( time > 999.0f, "was: " << time );
     TESTINFO( time < 1100.0f, "was: " << time );
 
     clock.reset();
-    TEST( !lock.set( 100.0f ));
+    TEST( !lock.set( 100 ));
     time = clock.getTimef();
 
     TESTINFO( time > 99.0f, "was: " << time );
