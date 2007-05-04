ced3ee834c6eff252b2776514e351a1b728295c0
Stefan Eilemann <eilemann@gmail.com>
2007-05-04 07:37:16 +0000

    Cleanup win32 defines
    [ ] May break build
    [ ] Breaks existing applications
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- lib/base/definesWin32.h
+++ lib/base/definesWin32.h
@@ -10,11 +10,20 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #  define WIN32_LEAN_AND_MEAN
 #endif
+//#ifndef EQ_BUILD_FOR_SDP
+//#  define EQ_BUILD_FOR_SDP
+//#endif
+#ifdef EQ_BUILD_FOR_SDP
+#  ifndef EQ_WIN32_SDP_JOIN_WAR // see thread.cxx
+#    define EQ_WIN32_SDP_JOIN_WAR
+#  endif
+#else
+#  ifndef EQ_USE_COMPRESSION
+#    define EQ_USE_COMPRESSION
+#  endif
+#endif
 #ifndef EQ_CHECK_THREADSAFETY
 #  define EQ_CHECK_THREADSAFETY
-#endif
-#ifndef EQ_USE_COMPRESSION
-#  define EQ_USE_COMPRESSION
 #endif
 #ifndef LITTLE_ENDIAN
 #  define LITTLE_ENDIAN
