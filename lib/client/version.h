bf1319ae413308855917577db906c12bdc99dbdc
Stefan Eilemann <eilemann@gmail.com>
2008-07-04 08:42:50 +0000

    0.5.3 release preparations
    [ ] May break build
    [ ] Breaks existing applications
    [ ] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [x] Documentation

--- lib/client/version.h
+++ lib/client/version.h
@@ -14,7 +14,7 @@ namespace eq
     // Equalizer version macros and functions
 #   define EQ_VERSION_MAJOR 0
 #   define EQ_VERSION_MINOR 5
-#   define EQ_VERSION_PATCH 2
+#   define EQ_VERSION_PATCH 3
 
     class Version
     {
