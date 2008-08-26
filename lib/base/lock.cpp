3cb94c4ed9cf4d1c76ca2051e7b1b65416082275
Stefan Eilemann <eilemann@gmail.com>
2008-08-26 12:33:05 +0000

    Switch to CriticalSection for Win32 locks
    [ ] May break build
    [ ] Breaks existing applications
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [x] Optimization
    [ ] Documentation

--- lib/base/lock.cpp
+++ lib/base/lock.cpp
@@ -18,51 +18,75 @@ namespace base
 class LockPrivate
 {
 public:
+#ifdef WIN32
+    CRITICAL_SECTION cs; 
+#else
     pthread_mutex_t mutex;
+#endif
 };
 
 Lock::Lock()
         : _data ( new LockPrivate )
 {
+#ifdef WIN32
+    InitializeCriticalSection( &_data->cs );
+#else
     const int error = pthread_mutex_init( &_data->mutex, 0 );
     if( error )
     {
         EQERROR << "Error creating pthread mutex: " << strerror(error) << endl;
         return;
     }
+#endif
     EQINFO << "New Lock @" << (void*)this << endl;
 }
 
 Lock::~Lock()
 {
+#ifdef WIN32
+    DeleteCriticalSection( &_data->cs ); 
+#else
     pthread_mutex_destroy( &_data->mutex );
+#endif
     delete _data;
     _data = 0;
 }
 
 void Lock::set()
 {
+#ifdef WIN32
+    EnterCriticalSection( &_data->cs );
+#else
     pthread_mutex_lock( &_data->mutex );
+#endif
 }
 
 
 void Lock::unset()
 {
+#ifdef WIN32
+    LeaveCriticalSection( &_data->cs );
+#else
     pthread_mutex_unlock( &_data->mutex );
+#endif
 }
 
 
 bool Lock::trySet()
 {
+#ifdef WIN32
+    return TryEnterCriticalSection( &_data->cs );
+#else
     return ( pthread_mutex_trylock( &_data->mutex ) == 0 );
+#endif
 }
 
 
 bool Lock::test()
 {
-    if( pthread_mutex_trylock( &_data->mutex ) == 0 )
+    if( trySet( ))
     {
-        pthread_mutex_unlock( &_data->mutex );
+        unset();
         return false;
     }
     return true;
--- lib/base/spinLock.cpp
+++ lib/base/spinLock.cpp
@@ -33,51 +33,75 @@ namespace base
 class SpinLockPrivate
 {
 public:
+#ifdef WIN32
+    CRITICAL_SECTION cs; 
+#else
     pthread_mutex_t mutex;
+#endif
 };
 
 SpinLock::SpinLock()
         : _data( new SpinLockPrivate )
 {
+#ifdef WIN32
+    InitializeCriticalSection( &_data->cs );
+#else
     const int error = pthread_mutex_init( &_data->mutex, 0 );
     if( error )
     {
         EQERROR << "Error creating pthread mutex: " << strerror(error) << endl;
         return;
     }
+#endif
     EQINFO << " New SpinLock @" << (void*)this << endl;
 }
 
 SpinLock::~SpinLock()
 {
+#ifdef WIN32
+    DeleteCriticalSection( &_data->cs ); 
+#else
     pthread_mutex_destroy( &_data->mutex );
+#endif
     delete _data;
     _data = 0;
 }
 
 void SpinLock::set()
 {
+#ifdef WIN32
+    EnterCriticalSection( &_data->cs );
+#else
     pthread_mutex_lock( &_data->mutex );
+#endif
 }
 
 
 void SpinLock::unset()
 {
+#ifdef WIN32
+    LeaveCriticalSection( &_data->cs );
+#else
     pthread_mutex_unlock( &_data->mutex );
+#endif
 }
 
 
 bool SpinLock::trySet()
 {
+#ifdef WIN32
+    return TryEnterCriticalSection( &_data->cs );
+#else
     return ( pthread_mutex_trylock( &_data->mutex ) == 0 );
+#endif
 }
 
 
 bool SpinLock::test()
 {
-    if( pthread_mutex_trylock( &_data->mutex ) == 0 )
+    if( trySet( ))
     {
-        pthread_mutex_unlock( &_data->mutex );
+        unset();
         return false;
     }
     return true;
