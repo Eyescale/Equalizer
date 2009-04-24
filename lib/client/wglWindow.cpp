1ca8668475825f3738d644b1ed4b7d793fe71980
Stefan Eilemann <eilemann@gmail.com>
2009-04-24 07:56:05 +0000

    WGL: Make fullscreen windows always-on-top
    [ ] May break build
    [ ] Breaks existing applications
    [ ] Bugfix
    [x] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- lib/client/wglWindow.cpp
+++ lib/client/wglWindow.cpp
@@ -3,9 +3,8 @@
                           , Makhinya Maxim
  *
  * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
  *  
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -421,6 +420,8 @@ HWND WGLWindow::_createWGLWindow( int pixelFormat, const PixelViewport& pvp  )
 
     if( getIAttribute( Window::IATTR_HINT_FULLSCREEN ) == ON )
     {
+        windowStyleEx |= WS_EX_TOPMOST;
+
         DEVMODE deviceMode = {0};
         deviceMode.dmSize = sizeof( DEVMODE );
         EnumDisplaySettings( 0, ENUM_CURRENT_SETTINGS, &deviceMode );
