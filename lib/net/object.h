c9c9ab8bc787cd2cfa257b39ba2f7403b21de11e
Stefan Eilemann <eilemann@gmail.com>
2009-05-15 07:38:15 +0000

    Bugfix: late object mapping causes assertion
    [ ] May break build
    [ ] Breaks existing applications
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- lib/net/object.h
+++ lib/net/object.h
@@ -2,9 +2,8 @@
 /* Copyright (c) 2005-2009, Stefan Eilemann <eile@equalizergraphics.com> 
  *
  * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
  *  
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -231,7 +230,7 @@ namespace net
          * @param version The new head version.
          */
         virtual void notifyNewHeadVersion( const uint32_t version )
-            { EQASSERT( version < getVersion() + 100 ); }
+            { EQASSERT( getVersion()==VERSION_NONE||version<getVersion()+100 );}
         //*}
 
         /** @name Methods used by session during mapping. */
