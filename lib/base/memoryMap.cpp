a3c04c09062021deb108c64c8dfc86b8f9cf3c9f
Stefan Eilemann <eilemann@gmail.com>
2009-06-29 09:40:36 +0000

    Add base::MemoryMap class
    [x] May break build
    [ ] Breaks existing applications
    [ ] Bugfix
    [x] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

---
+++ lib/base/memoryMap.cpp
@@ -0,0 +1,118 @@
+
+/* Copyright (c) 2009, Stefan Eilemann <eile@equalizergraphics.com> 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published 
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "memoryMap.h"
+
+namespace eq
+{
+namespace base
+{
+
+MemoryMap::MemoryMap()
+#ifdef WIN32
+        : _map( 0 )
+#else
+        : _fd( 0 )
+#endif
+        , _ptr( 0 )
+{}
+
+void* MemoryMap::map( const std::string& filename )
+{
+    if( _ptr )
+    {
+        EQWARN << "File already mapped" << std::endl;
+        return 0;
+    }
+
+#ifdef WIN32
+    // try to open binary file
+    HANDLE file = CreateFile( filename.c_str(), GENERIC_READ, FILE_SHARE_READ,
+                              0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0 );
+    if( file == INVALID_HANDLE_VALUE )
+    {
+        EQWARN << "Can't open " << filename << std::endl;
+        return 0;
+    }
+
+    // create a file mapping
+    _map = CreateFileMapping( file, 0, PAGE_READONLY, 0, 0, filename );
+    CloseHandle( file );
+    if( !map )
+    {
+        EQWARN << "File mapping failed." << std::endl;
+        return 0;
+    }
+    
+    // get a view of the mapping
+    _ptr = MapViewOfFile( map, FILE_MAP_READ, 0, 0, 0 );
+
+#else // POSIX
+
+    // try to open binary file
+    _fd = open( filename.c_str(), O_RDONLY );
+    if( fd < 0 )
+    {
+        EQWARN << "Can't open " << filename << std::endl;
+        return 0;
+    }
+    
+    // retrieving file information
+    struct stat status;
+    fstat( _fd, &status );
+    
+    // create memory mapped file
+    _ptr = mmap( 0, status.st_size, PROT_READ, MAP_SHARED, _fd, 0 );
+    if( _ptr == MAP_FAILED )
+    {
+        close( _fd );
+        _ptr = 0;
+        _fd = 0;
+    }
+#endif
+
+    return _ptr;
+}
+
+void MemoryMap::unmap()
+{
+    if( !_ptr )
+    {
+        EQWARN << "File not mapped" << std::endl;
+        return;
+    }
+
+#ifdef WIN32
+    UnmapViewOfFile( _ptr );
+    CloseHandle( _map );
+    
+    _ptr = 0;
+    _map = 0;
+#else
+    struct stat status;
+    fstat( _fd, &status );
+
+    munmap( _ptr, status.st__size );
+    close( _fd );
+    
+    _ptr = 0;
+    _fd = 0;
+#endif
+}
+
+}
+}
---
+++ lib/base/memoryMap.h
@@ -0,0 +1,61 @@
+
+/* Copyright (c) 2009, Stefan Eilemann <eile@equalizergraphics.com> 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published 
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef EQBASE_MEMORYMAP_H
+#define EQBASE_MEMORYMAP_H
+
+#include <eq/base/base.h>
+#include <string>
+namespace eq
+{
+namespace base
+{
+
+/** Helper to map a file to a memory address (mmap) */
+class MemoryMap 
+{
+public:
+    /** Construct a new memory mapper. */
+    EQ_EXPORT MemoryMap() : _memoryMap( 0 ) {}
+
+    /** 
+     * Map a file to a memory address.
+     *
+     * Currently the file is only mapped read-only.
+     *
+     * @param fileName The filename of the file to map.
+     * @return the pointer to the mapped file, or 0 upon error.
+     */
+    EQ_EXPORT void* map( const std::string& fileName );
+
+    /** Unmap the file. */
+    EQ_EXPORT void unmap();
+
+private:
+#ifdef WIN32
+    HANDLE _map;
+#else
+    int _fd;
+#endif
+
+    void* _ptr;
+};
+
+}
+}
+
+#endif //EQBASE_MEMORYMAP_H
