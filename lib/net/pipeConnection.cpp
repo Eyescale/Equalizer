a057de8d8b087e9dcc7be4e9f394dce8d59c2de7
Stefan Eilemann <eilemann@gmail.com>
2009-09-17 15:37:26 +0000

    (Re-)merge pipe connection implementations
    [x] May break Posix build
    [ ] Breaks existing applications
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- lib/net/pipeConnection.cpp
+++ lib/net/pipeConnection.cpp
@@ -15,8 +15,223 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include "pipeConnection.h"
+
+#include "connectionDescription.h"
+#include "node.h"
+
+#include <eq/base/log.h>
+#include <eq/base/thread.h>
+
+namespace eq
+{
+namespace net
+{
+
+PipeConnection::PipeConnection()
 #ifdef WIN32
-#  include "pipeConnectionWin32.cpp"
-#else
-#  include "pipeConnectionPosix.cpp"
+        : _readHandle( 0 ),
+          _writeHandle( 0 ),
+          _size( 0 ),
+          _dataPending( CreateEvent( 0, TRUE, FALSE, 0 ))
 #endif
+{
+    _description = new ConnectionDescription;
+    _description->type = CONNECTIONTYPE_PIPE;
+}
+
+PipeConnection::~PipeConnection()
+{
+    close();
+#ifdef WIN32
+    CloseHandle( _dataPending );
+#endif
+}
+
+#ifdef WIN32
+
+//----------------------------------------------------------------------
+// connect
+//----------------------------------------------------------------------
+bool PipeConnection::connect()
+{
+    EQASSERT( _description->type == CONNECTIONTYPE_PIPE );
+
+    if( _state != STATE_CLOSED )
+        return false;
+
+    _state = STATE_CONNECTING;
+    _size  = 0;
+
+    if( !_createPipe( ))
+    {
+        close();
+        return false;
+    }
+
+    _state = STATE_CONNECTED;
+    _fireStateChanged();
+    return true;
+}
+
+bool PipeConnection::_createPipe()
+{
+    if( CreatePipe( &_readHandle, &_writeHandle, 0, 0 ) == 0 )
+    {
+        EQERROR << "Could not create pipe: " 
+                << base::getErrorString( GetLastError( )) << std::endl;
+        close();
+        return false;
+    }
+    return true;
+}
+
+void PipeConnection::close()
+{
+    if( _readHandle )
+    {
+        CloseHandle( _readHandle );
+        _readHandle = 0;
+    }
+    if( _writeHandle )
+    {
+        CloseHandle( _writeHandle );
+        _writeHandle = 0;
+    }
+    _state = STATE_CLOSED;
+    _fireStateChanged();
+}
+void PipeConnection::readNB( void* buffer, const uint64_t bytes ) { /* NOP */ }
+int64_t PipeConnection::readSync( void* buffer, const uint64_t bytes )
+{
+    if( !_readHandle )
+        return -1;
+
+    DWORD bytesRead = 0;
+    const BOOL ret = ReadFile( _readHandle, buffer, static_cast<DWORD>( bytes ),
+                               &bytesRead, 0 );
+
+    if( ret == 0 ) // Error
+    {
+        EQWARN << "Error during read: " 
+               << base::getErrorString( GetLastError( )) << std::endl;
+        return -1;
+    }
+
+    if( bytesRead == 0 ) // EOF
+    {
+        close();
+        return -1;
+    }
+
+    _mutex.set();
+    EQASSERT( _size >= bytesRead );
+    _size -= bytesRead;
+    if( _size == 0 )
+        ResetEvent( _dataPending );
+    _mutex.unset();
+
+    return bytesRead;
+}
+
+int64_t PipeConnection::write( const void* buffer, const uint64_t bytes )
+{
+    if( !_writeHandle )
+        return -1;
+
+    const DWORD write = EQ_MIN( static_cast<DWORD>( bytes ), 4096 );
+
+    _mutex.set();
+    _size += write; // speculatively 'write' everything
+    _mutex.unset();
+
+    DWORD bytesWritten = 0;
+    const BOOL ret = WriteFile( _writeHandle, buffer, write, &bytesWritten, 0 );
+
+    if( ret == 0 ) // Error
+    {
+        EQWARN << "Error during write: "
+               << base::getErrorString( GetLastError( )) << std::endl;
+        bytesWritten = 0;
+    }
+
+    _mutex.set();
+    EQASSERT( _size >= write - bytesWritten );
+    _size -= (write - bytesWritten); // correct size
+
+    if( _size > 0 )
+        SetEvent( _dataPending );
+    else
+    {
+        EQASSERT( _size == 0 );
+        ResetEvent( _dataPending );
+    }
+    _mutex.unset();
+
+    if( ret==0 ) 
+        return -1;
+    return bytesWritten;
+}
+
+#else // !WIN32
+
+//----------------------------------------------------------------------
+// connect
+//----------------------------------------------------------------------
+bool PipeConnection::connect()
+{
+    EQASSERT( _description->type == CONNECTIONTYPE_PIPE );
+
+    if( _state != STATE_CLOSED )
+        return false;
+
+    _state = STATE_CONNECTING;
+
+    if( !_createPipe( ))
+    {
+        close();
+        return false;
+    }
+
+    EQVERB << "readFD " << _readFD << " writeFD " << _writeFD << std::endl;
+    _state = STATE_CONNECTED;
+    _fireStateChanged();
+    return true;
+}
+
+bool PipeConnection::_createPipe()
+{
+    int pipeFDs[2];
+    if( ::pipe( pipeFDs ) == -1 )
+    {
+        EQERROR << "Could not create pipe: " << strerror( errno );
+        close();
+        return false;
+    }
+
+    _readFD  = pipeFDs[0];
+    _writeFD = pipeFDs[1];
+    return true;
+}
+
+void PipeConnection::close()
+{
+    if( _readFD > 0 )
+    {
+        ::close(_readFD);
+        _readFD  = 0;
+    }
+
+    if( _writeFD > 0 )
+    {
+        ::close(_writeFD);
+        _writeFD = 0;
+    }
+
+    _state = STATE_CLOSED;
+    _fireStateChanged();
+}
+
+#endif
+}
+}
