21d7a467d198bd960161f75f430c9634561eb76b
Stefan Eilemann <eilemann@gmail.com>
2010-01-19 14:00:42 +0000

    Add missing files
    [ ] May break build
    [ ] May break existing applications
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

---
+++ lib/net/eventConnection.cpp
@@ -0,0 +1,116 @@
+
+/* Copyright (c) 2010, Stefan Eilemann <eile@equalizergraphics.com> 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "eventConnection.h"
+#include "pipeConnection.h"
+
+namespace eq
+{
+namespace net
+{
+EventConnection::EventConnection()
+#ifdef WIN32
+        : _event( 0 )
+#else
+        : _set( false )
+#endif
+{
+}
+
+EventConnection::~EventConnection()
+{
+    close();
+}
+
+bool EventConnection::connect()
+{
+    if( _state != STATE_CLOSED )
+        return false;
+
+    _state = STATE_CONNECTING;
+
+#ifdef WIN32
+    _event = CreateEvent( 0, TRUE, FALSE, 0 );
+#else
+    _connection = new PipeConnection;
+    EQCHECK( _connection->connect( ));
+    _set = false;
+#endif
+
+    _state = STATE_CONNECTED;
+    _fireStateChanged();
+    return true;    
+}
+
+void EventConnection::close()
+{
+#ifdef WIN32
+    if( _event )
+        CloseHandle( _event );
+    _event = 0;
+#else
+    if( _connection.isValid( ))
+        _connection->close();
+    _connection = 0;
+    _set = false;
+#endif
+
+    _state = STATE_CLOSED;
+    _fireStateChanged();
+}
+
+void EventConnection::set()
+{
+#ifdef WIN32
+    SetEvent( _event );
+#else
+    base::ScopedMutex mutex( _lock );
+    if( _set )
+        return;
+    
+    const char c = 42;
+    _connection->send( &c, 1, true );
+    _set = true;
+#endif
+}
+void EventConnection::reset()
+{
+#ifdef WIN32
+    ResetEvent( _event );
+#else
+    base::ScopedMutex mutex( _lock );
+    if( !_set )
+        return;
+    
+    char c = 42;
+    _connection->recvNB( &c, 1 );
+    _connection->recvSync( 0, 0 );
+    _set = false;
+#endif
+}
+
+Connection::Notifier EventConnection::getNotifier() const
+{
+#ifdef WIN32
+    return _event;
+#else
+    return _connection->getNotifier();
+#endif
+}
+
+}
+}
---
+++ lib/net/eventConnection.h
@@ -0,0 +1,67 @@
+
+/* Copyright (c) 2010, Stefan Eilemann <eile@equalizergraphics.com> 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef EQNET_EVENT_CONNECTION_H
+#define EQNET_EVENT_CONNECTION_H
+
+#include <eq/net/connection.h>   // base class
+
+namespace eq
+{
+namespace net
+{
+    /**
+     * A connection signalling an event.
+     *
+     * The connection is only useful to signal something to a ConnectionSet. No
+     * data can be read or written from it.
+     */
+    class EventConnection : public Connection
+    {
+    public:
+        EQ_EXPORT EventConnection();
+        EQ_EXPORT virtual ~EventConnection();
+
+        EQ_EXPORT virtual bool connect();
+        EQ_EXPORT virtual void close();
+
+        EQ_EXPORT void set();
+        EQ_EXPORT void reset();
+
+        EQ_EXPORT virtual Notifier getNotifier() const;
+
+    protected:
+        virtual void readNB( void* event, const uint64_t bytes )
+            { EQDONTCALL; }
+        virtual int64_t readSync( void* event, const uint64_t bytes )
+            { EQDONTCALL; return -1; }
+        EQ_EXPORT virtual int64_t write( const void*, const uint64_t )
+            { EQDONTCALL; return -1; }
+
+    private:
+#ifdef WIN32
+        HANDLE _event;
+#else
+        ConnectionPtr _connection;
+        base::Lock _lock;
+        bool _set;
+#endif
+    };
+}
+}
+
+#endif //EQNET_EVENT_CONNECTION_H
