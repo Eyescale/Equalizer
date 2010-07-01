28343198c7e87cd0b5aaf6d98b643e51c1542f9f
Cedric Stalder <cedric.stalder@gmail.com>
2010-07-01 13:06:49 +0000

    add utility for auto download Boos library
    [ ] May break build
    [ ] May break existing applications
    [ ] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [x] Optimization
    [ ] Documentation

---
+++ Windows/getBoost32.bat
@@ -0,0 +1,16 @@
+ECHO OFF
+Echo Downloading Boost
+Echo This may take some time, depending on your internet connection.
+ECHO ON
+
+bin\wget.exe http://downloads.sourceforge.net/project/boost/boost/1.43.0/boost_1_43_0.zip?use_mirror=ignum -O boost.zip
+
+bin\7z.exe x boost.zip
+
+rename boost_1_43_0 Boost
+
+del boost.zip
+
+cd Boost
+
+..\bin\bjam toolset=msvc-8.0 variant=debug,release link=static address-model=32 --stagedir=.\EQ_stage32 --with-system --with-date_time --with-regex stage
