60c6f3c130d2b348bfdc94099119e47d02b4f1c5
Stefan Eilemann <eilemann@gmail.com>
2010-10-28 08:38:52 +0000

    CMake: tweak copy script
    [ ] May break build
    [ ] Breaks existing applications
    [ ] Bugfix
    [x] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- cmake/copyScript.cmake
+++ cmake/copyScript.cmake
@@ -5,6 +5,10 @@ if(WIN32)
     if(NOT COPY_TARGET)
         set(COPY_TARGET "c:\\Equalizer" CACHE STRING "Target directory for the copyScript.bat")
     endif(NOT COPY_TARGET)
+    if(NOT COPY_TARGET_RELEASE_TYPE)
+        set(COPY_TARGET_RELEASE_TYPE Release CACHE STRING "Release configuration to copy (Release, RelWithDebInfo, MinSizeRel)")
+    endif(NOT COPY_TARGET_RELEASE_TYPE)
+
     if(CMAKE_SIZEOF_VOID_P MATCHES "8")
         set(PLATFORM_TYPE "x64\\")
     else(CMAKE_SIZEOF_VOID_P MATCHES "8")
@@ -24,42 +28,50 @@ if(WIN32)
         "xcopy /s /y \"${WIN_INSTALL_DIR}\\include\\\"* \"${COPY_TARGET}\\include\"\n\n"
         
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\eqServer.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\debug\\netperf.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\debug\\affinityCheck.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\eqServer.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\netperf.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\affinityCheck.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n\n"
-        
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\eqServer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\debug\\netperf.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\netperf.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\debug\\affinityCheck.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\eqServer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\netperf.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\affinityCheck.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n\n"
-        
+        "copy \"${WIN_BIN_DIR}\\bin\\debug\\affinityCheck.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\debug\\affinityCheck.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n\n"
+
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\Equalizer.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\Equalizer.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\Equalizer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\Equalizer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
-        "copy \"${WIN_BIN_DIR}\\lib\\debug\\Equalizer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\lib\\release\\Equalizer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}release\"\n\n"
-        
+        "copy \"${WIN_BIN_DIR}\\lib\\debug\\Equalizer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}debug\"\n\n"
+
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\EqualizerServer.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\EqualizerServer.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\EqualizerServer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\EqualizerServer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
-        "copy \"${WIN_BIN_DIR}\\server\\debug\\EqualizerServer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\server\\release\\EqualizerServer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}release\"\n\n"
-        
+        "copy \"${WIN_BIN_DIR}\\server\\debug\\EqualizerServer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}debug\"\n\n"
+
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\EqualizerAdmin.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\EqualizerAdmin.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
         "copy \"${WIN_BIN_DIR}\\bin\\debug\\EqualizerAdmin.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\bin\\release\\EqualizerAdmin.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
-        "copy \"${WIN_BIN_DIR}\\admin\\debug\\EqualizerAdmin.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}debug\"\n"
-        "copy \"${WIN_BIN_DIR}\\admin\\release\\EqualizerAdmin.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}release\"\n\n"
-                
+        "copy \"${WIN_BIN_DIR}\\admin\\debug\\EqualizerAdmin.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}debug\"\n\n"
+
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\eqServer.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\netperf.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\affinityCheck.exe\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n\n"
+
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\Equalizer.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\lib\\${COPY_TARGET_RELEASE_TYPE}\\Equalizer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\EqualizerServer.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\server\\${COPY_TARGET_RELEASE_TYPE}\\EqualizerServer.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\EqualizerAdmin.dll\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\admin\\${COPY_TARGET_RELEASE_TYPE}\\EqualizerAdmin.lib\" \"${COPY_TARGET}\\lib\\${PLATFORM_TYPE}release\"\n\n"
+    )
+
+  if(COPY_TARGET_RELEASE_TYPE MATCHES "RelWithDebInfo")
+    file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/copyScript.bat
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\eqServer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\netperf.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\affinityCheck.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\Equalizer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\EqualizerServer.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n"
+        "copy \"${WIN_BIN_DIR}\\bin\\${COPY_TARGET_RELEASE_TYPE}\\EqualizerAdmin.pdb\" \"${COPY_TARGET}\\bin\\${PLATFORM_TYPE}release\"\n\n"
+    )                
+  endif(COPY_TARGET_RELEASE_TYPE MATCHES "RelWithDebInfo")
+
+    file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/copyScript.bat
         "xcopy /s /y \"${WIN_SOURCE_DIR}\\server\\\"*.h \"${COPY_TARGET}\\include\\eq\\server\"\n"
-        "xcopy /s /y \"${WIN_SOURCE_DIR}\\server\\equalizers\\\"*.h \"${COPY_TARGET}\\include\\eq\\server\\equalizers\"\n\n"
+        "xcopy /s /y \"${WIN_SOURCE_DIR}\\server\\equalizers\\\"*.h \"${COPY_TARGET}\\include\\eq\\server\\equalizers\"\n"
         
         "\"%ProgramFiles%\\TortoiseSVN\\bin\\SubWCRev.exe\" \"${WIN_SOURCE_DIR}\" > \"${COPY_TARGET}\\include\\eq\\Version.txt\"\n"
         "pause\n"
