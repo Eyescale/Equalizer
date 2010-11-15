94d7f88b71b75678efe8865e9a7406d9f45aeb83
Stefan Eilemann <eilemann@gmail.com>
2010-11-15 10:46:57 +0000

    CMake: install missing example headers dos2unix
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [x] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- lib/net/base.h
+++ lib/net/base.h
@@ -1,4 +1,4 @@
- /* Copyright (c) 2010, Daniel Pfeifer <daniel@pfeifer-mail.de> 
+/* Copyright (c) 2010, Daniel Pfeifer <daniel@pfeifer-mail.de> 
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
