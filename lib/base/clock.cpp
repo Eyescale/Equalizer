b441034d7b508afd6e530629148cffd424a5aac1
Stefan Eilemann <eilemann@gmail.com>
2010-12-09 08:59:12 +0000

    Do not inline clock impl to avoid link issues
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

---
+++ lib/base/clock.cpp
@@ -0,0 +1,167 @@
+
+/* Copyright (c) 2010, Stefan Eilemann <eile@eyescale.ch> 
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
+ *  
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+#include "clock.h"
+
+namespace eq
+{
+namespace base
+{
+Clock::Clock() 
+{
+    reset();
+#ifdef Darwin
+    mach_timebase_info( &_timebaseInfo );
+#elif defined (_WIN32)
+    QueryPerformanceFrequency( &_frequency );
+#endif
+}
+
+void Clock::reset()
+{
+#ifdef Darwin
+    _start = mach_absolute_time();
+#elif defined (_WIN32)
+    QueryPerformanceCounter( &_start );
+#else
+    clock_gettime( CLOCK_REALTIME, &_start );
+#endif
+}
+
+void Clock::set( const int64_t time )
+{
+    reset();
+#ifdef Darwin
+    _start -= static_cast< uint64_t >(
+        time * _timebaseInfo.denom / _timebaseInfo.numer *
+                                     1000000 );
+#elif defined (_WIN32)
+    _start.QuadPart -= static_cast<long long>( 
+        time * _frequency.QuadPart / 1000 );
+#else
+    const int sec   = static_cast< int >( time / 1000 ) + 1;
+    _start.tv_sec  -= sec;
+    _start.tv_nsec -= static_cast<int>(
+        (time - sec * 1000) * 1000000 );
+    if( _start.tv_nsec > 1000000000 )
+    {
+        _start.tv_sec  += 1;
+        _start.tv_nsec -= 1000000000;
+    }
+#endif
+}
+
+float Clock::getTimef() const
+{
+#ifdef Darwin
+    const int64_t elapsed = mach_absolute_time() - _start;
+    return ( elapsed * _timebaseInfo.numer / _timebaseInfo.denom /
+             1000000.f );
+#elif defined (_WIN32)
+    LARGE_INTEGER now;
+    QueryPerformanceCounter( &now );
+    return 1000.0f * (now.QuadPart - _start.QuadPart) / 
+        _frequency.QuadPart;
+#else
+    struct timespec now;
+    clock_gettime( CLOCK_REALTIME, &now );
+    return ( 1000.0f * (now.tv_sec - _start.tv_sec) +
+             0.000001f * (now.tv_nsec - _start.tv_nsec));
+#endif
+}
+
+float Clock::resetTimef()
+{
+#ifdef Darwin
+    const uint64_t now = mach_absolute_time();
+    const int64_t elapsed = now - _start;
+    const float time = elapsed * _timebaseInfo.numer /
+        _timebaseInfo.denom / 1000000.f;
+#elif defined (_WIN32)
+    LARGE_INTEGER now;
+    QueryPerformanceCounter( &now );
+    const float time = 1000.0f * (now.QuadPart - _start.QuadPart) / 
+        _frequency.QuadPart;
+#else
+    struct timespec now;
+    clock_gettime( CLOCK_REALTIME, &now );
+    const float time = ( 1000.0f * (now.tv_sec - _start.tv_sec) +
+                         0.000001f * (now.tv_nsec - _start.tv_nsec));
+#endif
+    _start = now;
+    return time;
+}
+
+int64_t Clock::getTime64() const
+{
+#ifdef Darwin
+    const int64_t elapsed = mach_absolute_time() - _start;
+    return elapsed * _timebaseInfo.numer / _timebaseInfo.denom /
+        1000000;
+#elif defined (_WIN32)
+    LARGE_INTEGER now;
+    QueryPerformanceCounter( &now );
+    return 1000 * (now.QuadPart - _start.QuadPart) /
+        _frequency.QuadPart;
+#else
+    struct timespec now;
+    clock_gettime( CLOCK_REALTIME, &now );
+    return ( 1000 * (now.tv_sec - _start.tv_sec) +
+             static_cast< int64_t >(0.000001f * 
+                                    (now.tv_nsec-_start.tv_nsec)));
+#endif
+}
+
+double Clock::getTimed() const
+{
+#ifdef Darwin
+    const int64_t elapsed = mach_absolute_time() - _start;
+    return ( elapsed * _timebaseInfo.numer / _timebaseInfo.denom /
+             1000000. );
+#elif defined (_WIN32)
+    LARGE_INTEGER now;
+    QueryPerformanceCounter( &now );
+    return 1000.0 * (now.QuadPart - _start.QuadPart) /
+        _frequency.QuadPart;
+#else
+    struct timespec now;
+    clock_gettime( CLOCK_REALTIME, &now );
+    return ( 1000.0 * (now.tv_sec - _start.tv_sec) +
+             0.000001 * (now.tv_nsec - _start.tv_nsec));
+#endif
+}
+
+float Clock::getMilliSecondsf() const
+{
+#if defined (Darwin) || defined (_WIN32)
+    double time = getTimed();
+    return static_cast<float>
+        (time - static_cast<unsigned>(time/1000.) * 1000);
+#else
+    struct timespec now;
+    clock_gettime( CLOCK_REALTIME, &now );
+
+    if( now.tv_nsec < _start.tv_nsec )
+        return ( 1000.f + 0.000001f*(now.tv_nsec - _start.tv_nsec));
+                
+    return ( 0.000001f * ( now.tv_nsec - _start.tv_nsec ));
+#endif
+}
+
+}
+}
--- lib/base/clock.h
+++ lib/base/clock.h
@@ -41,15 +41,7 @@ namespace base
     {
     public :
         /** Construct a new clock. @version 1.0 */
-        Clock() 
-            {
-                reset();
-#ifdef Darwin
-                mach_timebase_info( &_timebaseInfo );
-#elif defined (_WIN32)
-                QueryPerformanceFrequency( &_frequency );
-#endif
-            }
+        Clock();
 
         /** Destroy the clock. @version 1.0 */
         ~Clock() {}
@@ -58,137 +50,35 @@ namespace base
          * Reset the base time of the clock to the current time.
          * @version 1.0
          */
-        void reset()
-            {
-#ifdef Darwin
-                _start = mach_absolute_time();
-#elif defined (_WIN32)
-                QueryPerformanceCounter( &_start );
-#else
-                clock_gettime( CLOCK_REALTIME, &_start );
-#endif
-            }
+        void reset();
 
         /** Set the current time of the clock. @version 1.0 */
-        void set( const int64_t time )
-            {
-                reset();
-#ifdef Darwin
-                _start -= static_cast< uint64_t >(
-                              time * _timebaseInfo.denom / _timebaseInfo.numer *
-                                     1000000 );
-#elif defined (_WIN32)
-                _start.QuadPart -= static_cast<long long>( 
-                    time * _frequency.QuadPart / 1000 );
-#else
-                const int sec   = static_cast< int >( time / 1000 ) + 1;
-                _start.tv_sec  -= sec;
-                _start.tv_nsec -= static_cast<int>(
-                    (time - sec * 1000) * 1000000 );
-                if( _start.tv_nsec > 1000000000 )
-                {
-                    _start.tv_sec  += 1;
-                    _start.tv_nsec -= 1000000000;
-                }
-#endif
-            }
+        void set( const int64_t time );
 
         /** 
          * @return the elapsed time in milliseconds since the last clock reset.
          * @version 1.0
          */
-        float getTimef() const
-            {
-#ifdef Darwin
-                const int64_t elapsed = mach_absolute_time() - _start;
-                return ( elapsed * _timebaseInfo.numer / _timebaseInfo.denom /
-                         1000000.f );
-#elif defined (_WIN32)
-                LARGE_INTEGER now;
-                QueryPerformanceCounter( &now );
-                return 1000.0f * (now.QuadPart - _start.QuadPart) / 
-                    _frequency.QuadPart;
-#else
-                struct timespec now;
-                clock_gettime( CLOCK_REALTIME, &now );
-                return ( 1000.0f * (now.tv_sec - _start.tv_sec) +
-                         0.000001f * (now.tv_nsec - _start.tv_nsec));
-#endif
-            }
+        float getTimef() const;
 
         /** 
          * @return the elapsed time in milliseconds since the last clock reset
          *         and atomically reset the clock.
          * @version 1.0
          */
-        float resetTimef()
-            {
-#ifdef Darwin
-                const uint64_t now = mach_absolute_time();
-                const int64_t elapsed = now - _start;
-                const float time = elapsed * _timebaseInfo.numer /
-                                  _timebaseInfo.denom / 1000000.f;
-#elif defined (_WIN32)
-                LARGE_INTEGER now;
-                QueryPerformanceCounter( &now );
-                const float time = 1000.0f * (now.QuadPart - _start.QuadPart) / 
-                                   _frequency.QuadPart;
-#else
-                struct timespec now;
-                clock_gettime( CLOCK_REALTIME, &now );
-                const float time = ( 1000.0f * (now.tv_sec - _start.tv_sec) +
-                                    0.000001f * (now.tv_nsec - _start.tv_nsec));
-#endif
-                _start = now;
-                return time;
-            }
+        float resetTimef();
 
         /** 
          * @return the elapsed time in milliseconds since the last clock reset.
          * @version 1.0
          */
-        int64_t getTime64() const
-            {
-#ifdef Darwin
-                const int64_t elapsed = mach_absolute_time() - _start;
-                return elapsed * _timebaseInfo.numer / _timebaseInfo.denom /
-                       1000000;
-#elif defined (_WIN32)
-                LARGE_INTEGER now;
-                QueryPerformanceCounter( &now );
-                return 1000 * (now.QuadPart - _start.QuadPart) /
-                       _frequency.QuadPart;
-#else
-                struct timespec now;
-                clock_gettime( CLOCK_REALTIME, &now );
-                return ( 1000 * (now.tv_sec - _start.tv_sec) +
-                         static_cast< int64_t >(0.000001f * 
-                                                 (now.tv_nsec-_start.tv_nsec)));
-#endif
-            }
+        int64_t getTime64() const;
 
         /** 
          * @return the elapsed time in milliseconds since the last clock reset.
          * @version 1.0
          */
-        double getTimed() const
-            {
-#ifdef Darwin
-                const int64_t elapsed = mach_absolute_time() - _start;
-                return ( elapsed * _timebaseInfo.numer / _timebaseInfo.denom /
-                         1000000. );
-#elif defined (_WIN32)
-                LARGE_INTEGER now;
-                QueryPerformanceCounter( &now );
-                return 1000.0 * (now.QuadPart - _start.QuadPart) /
-                                                            _frequency.QuadPart;
-#else
-                struct timespec now;
-                clock_gettime( CLOCK_REALTIME, &now );
-                return ( 1000.0 * (now.tv_sec - _start.tv_sec) +
-                         0.000001 * (now.tv_nsec - _start.tv_nsec));
-#endif
-            }
+        double getTimed() const;
 
         /** 
          * Returns the millisecond part of the time elapsed since the last
@@ -199,22 +89,7 @@ namespace base
          * @return the millisecond part of the time elapsed. 
          * @version 1.0
          */
-        float getMilliSecondsf() const
-            {
-#if defined (Darwin) || defined (_WIN32)
-                double time = getTimed();
-                return static_cast<float>
-                    (time - static_cast<unsigned>(time/1000.) * 1000);
-#else
-                struct timespec now;
-                clock_gettime( CLOCK_REALTIME, &now );
-
-                if( now.tv_nsec < _start.tv_nsec )
-                    return ( 1000.f + 0.000001f*(now.tv_nsec - _start.tv_nsec));
-                
-                return ( 0.000001f * ( now.tv_nsec - _start.tv_nsec ));
-#endif
-            }
+        float getMilliSecondsf() const;
 
     private:
 #ifdef Darwin
