a885acc19cf307ed98d76a088a3e51ea24456934
Stefan Eilemann <eilemann@gmail.com>
2011-01-21 11:17:20 +0000

    Minor cleanups
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- libs/client/image.cpp
+++ libs/client/image.cpp
@@ -370,7 +370,7 @@ void Image::readback( const Frame::Buffer buffer, const util::Texture* texture,
     else
         downloader->download( _pvp, 0 , flags, memory.pvp, &memory.pixels );
 
-    EQ_GL_CALL( downloader->setGLEWContext( 0 ) );
+    downloader->setGLEWContext( 0 );
     memory.state = Memory::VALID;
 }
 
