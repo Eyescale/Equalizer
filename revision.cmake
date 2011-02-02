324e5e478d1219a462a7dcf6f0938b8c232d3a4c
Stefan Eilemann <eilemann@gmail.com>
2011-02-02 07:43:41 +0000

    Tweak version script
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- revision.cmake
+++ revision.cmake
@@ -6,8 +6,12 @@ if(Subversion_FOUND)
   EXECUTE_PROCESS(COMMAND ${Subversion_SVN_EXECUTABLE} info ${CMAKE_SOURCE_DIR}
     OUTPUT_VARIABLE SVN_INFO_OUTPUT ERROR_VARIABLE SVN_INFO_ERROR)
 
-  if(NOT SVN_INFO_ERROR)
+  if(SVN_INFO_ERROR)
+    message(STATUS "No revision version support, subversion error: ${SVN_INFO_ERROR}")
+  else()
     Subversion_WC_INFO(${CMAKE_SOURCE_DIR} EQ_SVN)
     set(EQ_REVISION ${EQ_SVN_WC_REVISION})
   endif()
+else()
+  message(STATUS "No revision version support, subversion not found")
 endif(Subversion_FOUND)
