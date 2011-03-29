feaad9c2912da7993ac26a80a365973f40ffb14d
Stefan Eilemann <eilemann@gmail.com>
2011-03-29 10:01:03 +0200

    Git toolchain updates
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- revision.cmake
+++ revision.cmake
@@ -1,18 +1,12 @@
 # Copyright (c) 2011 Stefan Eilemann <eile@eyescale.ch>
 
-find_package(Subversion)
-
-if(Subversion_FOUND)
-  EXECUTE_PROCESS(COMMAND ${Subversion_SVN_EXECUTABLE} info ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE SVN_INFO_OUTPUT ERROR_VARIABLE SVN_INFO_ERROR)
-
-  if(SVN_INFO_ERROR)
-    message(STATUS "No revision version support, subversion error: ${SVN_INFO_ERROR}")
-  else()
-    Subversion_WC_INFO(${CMAKE_SOURCE_DIR} EQ_SVN)
-    set(EQ_REVISION ${EQ_SVN_WC_REVISION})
+IF(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/.git)
+  FIND_PACKAGE(Git)
+  IF(Git_FOUND)
+    Git_WC_INFO(${CMAKE_CURRENT_SOURCE_DIR} EQ)
+    SET(EQ_REVISION ${EQ_WC_REVISION_HASH})
     message(STATUS "  Revision ${EQ_REVISION}")
-  endif()
-else()
-  message(STATUS "No revision version support, subversion not found")
-endif(Subversion_FOUND)
+  ELSE()
+    message(STATUS "No revision version support, git not found")
+  ENDIF()
+ENDIF()
