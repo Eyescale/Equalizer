1e8eae8441c76fc5077bf47c9def7df7cffbb60b
delyas <carsten.rohn@rtt.ag>
2011-04-19 17:27:27 +0800

    belongs to last commit:
    - logging is now thread safe
    - improved barrier topic logging
    
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [x] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- libs/collage/base/log.h
+++ libs/collage/base/log.h
@@ -28,6 +28,7 @@
 #define COBASE_LOG_H
 
 #include <co/base/api.h>
+#include <co/base/lock.h>
 
 #include <assert.h>
 #include <iomanip>
@@ -96,18 +97,7 @@ namespace base
     protected:
         virtual int_type overflow( LogBuffer::int_type c );
         
-        virtual int sync() 
-            {
-                if( !_blocked )
-                {
-                    const std::string& string = _stringStream.str();
-                    _stream.write( string.c_str(), string.length( ));
-                    _stream.rdbuf()->pubsync();
-                    _stringStream.str( "" );
-                }
-                _newLine = true;
-                return 0;
-            }
+        virtual int sync();
 
     private:
         LogBuffer( const LogBuffer& );
