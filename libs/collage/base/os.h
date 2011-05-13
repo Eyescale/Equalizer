810724cb4e8b8b9858db01ccda0e6e14783fb191
Cedric Stalder <cedric.stalder@gmail.com>
2011-05-13 10:47:21 +0200

    Fix Win32 build
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- libs/collage/base/os.h
+++ libs/collage/base/os.h
@@ -36,7 +36,7 @@
 #    define NOMINMAX
 #  endif
 #  ifndef _WIN32_WINNT // Hopefully to higher than 0x500...
-#    define _WIN32_WINNT 0x500 // >= 2000, for WM_XBUTTONDOWN and others
+#    define _WIN32_WINNT 0x501 // > 2000, for WM_XBUTTONDOWN and others
 #  endif
 #  include <winsock2.h>
 #  include <windows.h>
