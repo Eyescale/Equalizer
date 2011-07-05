1ec19b571162fe5a806317dd329b6ecaf4815a24
Stefan Eilemann <eilemann@gmail.com>
2011-07-05 10:45:14 +0200

    make rw lock test output easier to import as csv
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- tests/base/rwLock.cpp
+++ tests/base/rwLock.cpp
@@ -93,10 +93,15 @@ template< class T > void _test()
     WriteThread< T > writers[MAXTHREADS];
     ReadThread< T > readers[MAXTHREADS];
 
-    for( size_t nThreads = 1; nThreads <= MAXTHREADS; nThreads = nThreads<<1 )
+    std::cout << "               Class, write ops/ms,  read ops/ms, w threads, r threads"
+              << std::endl;
+    for( size_t nWrite = 1; nWrite <= MAXTHREADS; nWrite = nWrite<<1 )
     {
-        for( size_t nWrite = nThreads; nWrite > 0; nWrite = nWrite>>1 )
+        for( size_t nThreads = 1; nThreads <= MAXTHREADS; nThreads=nThreads<<1 )
         {
+            if( nThreads < nWrite )
+                continue;
+
             const size_t nRead = nThreads - nWrite;
             _clock.reset();
             for( size_t i = 0; i < nWrite; ++i )
@@ -109,7 +114,7 @@ template< class T > void _test()
             {
                 readers[i].lock = lock;
                 readers[i].nLoops = 0;
-                TEST( readers[i].start( ));
+                TESTINFO( readers[i].start(), i );
             }
             co::base::sleep( 100 ); // let threads initialize
 
@@ -133,11 +138,12 @@ template< class T > void _test()
             for( size_t i=0; i<nRead; ++i )
                 nReadOps += readers[i].nLoops;
 
-            std::cout << co::base::className( lock ) << ", "
-                      << 3 * NOPS * nWriteOps * nWrite / time
-                      << "w + " << 3 * NOPS * nReadOps * nRead / time
-                      << "r lock ops/ms, " << nWrite << "w+" << nRead
-                      << "r threads" << std::endl;
+            std::cout << std::setw(20)<< co::base::className( lock ) << ", "
+                      << std::setw(12) << 3 * NOPS * nWriteOps * nWrite / time
+                      << ", " << std::setw(12)
+                      << 3 * NOPS * nReadOps * nRead / time << ", " 
+                      << std::setw(9) << nWrite << ", " << std::setw(9)
+                      << nRead << std::endl;
         }
         std::cout << std::endl;
     }
