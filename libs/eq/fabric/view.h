5c6289c48b5ef3541bc1f6d2797456ab5771f448
Stefan Eilemann <eilemann@gmail.com>
2011-10-25 14:09:58 +0200

    Fix Windows build
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- libs/eq/fabric/view.h
+++ libs/eq/fabric/view.h
@@ -90,7 +90,7 @@ namespace fabric
         EQFABRIC_INL void setViewport( const Viewport& viewport );
 
         /** @internal Get the mode of this view. */
-        Mode getMode( ) const { return _mode; }
+        Mode getMode() const { return _mode; }
         
         /**
          * Set the mode of this view.
@@ -109,7 +109,7 @@ namespace fabric
         virtual void activateMode( const Mode mode ){ _mode = mode; }
 
         /** @return true if the view's layout is active. @version 1.1.5 */
-        bool isActive() const;
+        EQFABRIC_INL bool isActive() const;
         //@}
 
         /** @name Operations */
