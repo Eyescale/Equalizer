6bc8a33b5bc18c1773bbf456f579b9947a0f5692
MichaelVlad <mpope@chyron.com>
2011-11-02 16:43:46 -0300

    Update examples/eqPly/node.cpp

--- examples/eqPly/node.cpp
+++ examples/eqPly/node.cpp
@@ -30,11 +30,78 @@
 
 #include "config.h"
 #include "error.h"
+#include "process.h"
 
+class Distributable : public co::Serializable
+{
+public:
+    Distributable(eqPly::Node* parent);
+	virtual void notifyNewHeadVersion(const eq::uint128_t& version);
+	//the version. we will need this to avoid deadlock (for reasons I don't understand).
+	eq::uint128_t v;
+protected:
+private:
+	eqPly::Node* node;
+};
+
+//MVP: This thread simulates a worker thread. It syncs the Distributable and does some work.
+static void Worker(void* p)
+{
+	Distributable* slave = reinterpret_cast<Distributable*>(p);
+	//MVP: Uncomment this line to prevent deadlocking.
+	//if(slave->v != co::VERSION_FIRST)
+	slave->sync();
+	//do some work
+	Sleep(500);
+}
+
+Distributable::Distributable(eqPly::Node* parent)
+{
+	node = parent;
+}
+void Distributable::notifyNewHeadVersion(const eq::uint128_t& version)
+{
+	v = version;
+	::_beginthread(Worker,0,this);
+}
 namespace eqPly
 {
+	//MVP: This thread simulates the client UI thread. We add a new Distributable every quarter of a second and map it.
+static void PeriodicMap(void* p)
+{
+	eqPly::Node* Node = reinterpret_cast<eqPly::Node*>(p);
+	eq::Config* config = Node->getConfig();
+	UINT iter = 0;
+	while(true)
+	{
+		Sleep(250);
+		
+		char c[128];
+		sprintf(c,"iteration: %d\n",++iter);
+		OutputDebugString((LPCSTR)c);
+
+		Distributable* master_thing = new Distributable(Node);
+		
+		OutputDebugString("Registering master...\n");
+		config->registerObject(master_thing);
+		OutputDebugString("Master registered.\n");
+
+
+		Distributable* slave_thing = new Distributable(Node);
+
+		OutputDebugString("Mapping slave...\n");
+		bool bMapped = config->mapObject(slave_thing,master_thing->getID());
+		if(bMapped)
+			OutputDebugString("Slave mapped.\n");
+		else
+			OutputDebugString("Map failed.\n");
+	}
+}
+
 bool Node::configInit( const eq::uint128_t& initID )
 {
+	::_beginthread(PeriodicMap,0,(void*)this);
+
     if( !eq::Node::configInit( initID ))
         return false;
 
