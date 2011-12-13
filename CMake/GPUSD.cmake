e4bfcd744093d34230eefc0c3fa1dfa8250180fa
Stefan Eilemann <eilemann@gmail.com>
2011-12-13 14:38:28 +0100

    Fix GPUSD inclusion, bump to 1.0.1
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- CMake/GPUSD.cmake
+++ CMake/GPUSD.cmake
@@ -1,9 +1,9 @@
 
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_INSTALL_PREFIX}/share/gpu-sd/CMake)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/gpu-sd/CMake)
-find_package(GPUSD 1.0.0)
+find_package(GPUSD 1.0.1)
 
-if(EQUALIZER_RELEASE)
+if(EQUALIZER_RELEASE OR (GPUSD_FOUND AND NOT GPUSD_LOCAL))
   return()
 endif()
 
@@ -14,16 +14,9 @@ else()
   message(STATUS "git not found, automatic submodule configuration not done")
 endif()
 
-if(GPUSD_FOUND)
-  if(GPUSD_LOCAL)
-    add_subdirectory(gpu-sd)
-  endif()
-  return()
-endif()
-
 if(EXISTS "${CMAKE_SOURCE_DIR}/gpu-sd/CMakeLists.txt")
   add_subdirectory(gpu-sd)
-  find_package(GPUSD)
+  find_package(GPUSD) # re-find after add_subdirectory to find correct modules
 else()
   message(WARNING "git submodule update failed, no automatic configuration")
 endif()
