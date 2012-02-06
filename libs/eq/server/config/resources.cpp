05c19a3c43e20c1d4a88ccb71ebdfd945fc900eb
Stefan Eilemann <eilemann@gmail.com>
2012-02-06 09:57:58 +0100

    Simplification of auto config frame creation
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- libs/eq/server/config/resources.cpp
+++ libs/eq/server/config/resources.cpp
@@ -470,7 +470,9 @@ Compound* Resources::_addDSCompound( Compound* root, const Channels& channels )
                     vp = Viewport( 0.f, static_cast< float >( y )/100000.f,
                                   1.f, static_cast< float >( step )/100000.f );
 
-                eq::server::Frame* outputFrame = _createFrame( frameName, vp );
+                eq::server::Frame* outputFrame = new eq::server::Frame;
+                outputFrame->setName( frameName.str( ));
+                outputFrame->setViewport( vp );
                 outputFrame->setBuffers( eq::Frame::BUFFER_COLOR |
                                          eq::Frame::BUFFER_DEPTH );
                 drawChild->addOutputFrame( outputFrame );
@@ -480,7 +482,9 @@ Compound* Resources::_addDSCompound( Compound* root, const Channels& channels )
                 frameName << "tile" << i - children.begin() << ".channel"
                           << j - children.begin();
 
-                child->addInputFrame( _createFrame( frameName ));
+                eq::server::Frame* inputFrame = new eq::server::Frame;
+                inputFrame->setName( frameName.str( ));
+                child->addInputFrame( inputFrame );
             }
             // else own tile, is in place
 
@@ -540,29 +544,6 @@ const Compounds& Resources::_addSources( Compound* compound,
     }
 
     return compound->getChildren();
-}
-
-eq::server::Frame* Resources::_createFrame( const char* name )
-{
-  eq::server::Frame* frame = new eq::server::Frame;
-  frame->setName( std::string( name ));
-  return frame;
-}
-
-eq::server::Frame* Resources::_createFrame( std::ostringstream& name )
-{
-  return _createFrame( name.str().c_str( ));
-}
-
-eq::server::Frame* Resources::_createFrame( std::ostringstream& name,
-                                       const eq::fabric::Viewport& vp,
-                                       bool colorOnly )
-{
-  eq::server::Frame* frame = _createFrame( name );
-  frame->setViewport( vp );
-  if( colorOnly )
-      frame->setBuffers( eq::Frame::BUFFER_COLOR );
-  return frame;
 }
 
 }
