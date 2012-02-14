8e8be9e843e4d2333e1ea6d2685f5b10fe1accc5
Stefan Eilemann <eilemann@gmail.com>
2012-02-14 08:30:22 +0100

    Minor cleanups
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [ ] Bugfix
    [ ] New Feature
    [x] Cleanup
    [ ] Optimization
    [ ] Documentation

--- libs/co/instanceCache.h
+++ libs/co/instanceCache.h
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2009-2011, Stefan Eilemann <eile@equalizergraphics.com> 
+/* Copyright (c) 2009-2012, Stefan Eilemann <eile@equalizergraphics.com> 
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -22,6 +22,7 @@
 #include <co/types.h>
 
 #include <co/base/clock.h>     // member
+#include <co/base/lock.h>      // member
 #include <co/base/lockable.h>  // member
 #include <co/base/stdExt.h>    // member
 #include <co/base/thread.h>    // member
