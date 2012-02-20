Eyescale/Equalizer

commit 2035bb79d758431b7a73bf088d7bfe4f2263ed3d

Fixed msvc10 warning
[ ] May break build
[ ] May break existing applications (see CHANGES.txt)
[x] Bugfix
[ ] New Feature
[ ] Cleanup
[ ] Optimization
[ ] Documentation

libs/eq/server/view.cpp
@@ -398,7 +398,7 @@ float View::_computeFocusRatio( Vector3f& eye )
     if( channels.empty( ))
         return 1.f;
 
-    Vector4f view4 = Vector3f::FORWARD;
+    Vector4f view4 = Vector4f::FORWARD;
     if( mode == FOCUSMODE_RELATIVE_TO_OBSERVER )
     {
         view4 = observer->getHeadMatrix() * view4;

commit bf3512eeba386890fdf894f41219e39a21cd7f59

Fix focus distance support, broken by 2035bb79d758431b7a73bf088d7bfe4f2263ed3d
[ ] May break build
[ ] May break existing applications (see CHANGES.txt)
[x] Bugfix
[ ] New Feature
[ ] Cleanup
[ ] Optimization
[ ] Documentation

libs/eq/server/view.cpp
@@ -443,7 +443,7 @@ float View::_computeFocusRatio( Vector3f& eye )
     if( channels.empty( ))
         return 1.f;
 
-    Vector4f view4 = Vector4f::FORWARD;
+    Vector4f view4( Vector3f::FORWARD );
     if( mode == FOCUSMODE_RELATIVE_TO_OBSERVER )
     {
         view4 = observer->getHeadMatrix() * view4;