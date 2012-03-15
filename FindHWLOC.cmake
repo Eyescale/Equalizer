68161c3cd6c1d57391aa6d30ada0ff21fe1fd54c
Stefan Eilemann <eilemann@gmail.com>
2012-03-15 16:19:36 +0100

    Fix hwloc build and find script
    [ ] May break build
    [ ] May break existing applications (see CHANGES.txt)
    [x] Bugfix
    [ ] New Feature
    [ ] Cleanup
    [ ] Optimization
    [ ] Documentation

--- FindHWLOC.cmake
+++ FindHWLOC.cmake
@@ -7,17 +7,15 @@ find_path(HWLOC_INCLUDE_DIR "hwloc.h"
   /opt/local/include 
 )
 
-find_library(HWLOC_LIB NAMES hwloc 
-	HINTS ${HWLOC_ROOT}
-	PATHS 
-	/usr/lib 
-	/usr/local/lib 
-	/opt/local/lib
+find_library(HWLOC_LIBRARIES NAMES hwloc 
+  HINTS ${HWLOC_ROOT}/lib
+  PATHS /usr/lib /usr/local/lib /opt/local/lib
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(HWLOC DEFAULT_MSG HWLOC_LIB HWLOC_INCLUDE_DIR)
+find_package_handle_standard_args(HWLOC DEFAULT_MSG HWLOC_LIBRARIES
+  HWLOC_INCLUDE_DIR)
 
 if(HWLOC_FOUND)
-  message(STATUS "Found HWLOC in ${HWLOC_INCLUDE_DIR};${HWLOC_LIB}")
+  message(STATUS "Found HWLOC in ${HWLOC_INCLUDE_DIR};${HWLOC_LIBRARIES}")
 endif(HWLOC_FOUND)
