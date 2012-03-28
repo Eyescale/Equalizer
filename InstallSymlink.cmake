bf7ee2a1ac4484e86be49c8204e6c859dc958518
Stefan Eilemann <eilemann@gmail.com>
2012-03-28 11:39:44 +0200

    Tweak InstallSymlink

--- InstallSymlink.cmake
+++ InstallSymlink.cmake
@@ -5,14 +5,16 @@ function(INSTALL_SYMLINK FROM TO COMPONENT)
     install(CODE
       "get_filename_component(FROM_ABSOLUTE ${CMAKE_INSTALL_PREFIX}/${FROM}
          ABSOLUTE)
-       execute_process(COMMAND mlink \${FROM_ABSOLUTE} ${TO})"
+       execute_process(COMMAND mlink \${FROM_ABSOLUTE}
+         ${CMAKE_INSTALL_PREFIX}/${TO})"
       COMPONENT "${COMPONENT}")
   else()
     install(CODE
-      "execute_process(COMMAND rm -f ${TO})
+      "execute_process(COMMAND rm -f ${CMAKE_INSTALL_PREFIX}/${TO})
        get_filename_component(FROM_ABSOLUTE ${CMAKE_INSTALL_PREFIX}/${FROM}
          ABSOLUTE)
        execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink  
-         \${FROM_ABSOLUTE} ${TO})" COMPONENT "${COMPONENT}")
+         \${FROM_ABSOLUTE} ${CMAKE_INSTALL_PREFIX}/${TO})"
+      COMPONENT "${COMPONENT}")
   endif()
 endfunction()
