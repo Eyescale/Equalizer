5e2eda6a14392d52a79c7d071ca599416b731fec
Stefan Eilemann <eilemann@gmail.com>
2012-04-17 12:29:35 +0200

    Extend Common module

--- Common.cmake
+++ Common.cmake
@@ -35,9 +35,14 @@ add_definitions(-DBOOST_ALL_NO_LIB) # Don't use 'pragma lib' on Windows
 
 # Compiler setting
 if(CMAKE_COMPILER_IS_GNUCXX)
+  include(EqCompilerVersion)
+  EQ_COMPILER_DUMPVERSION(GCC_COMPILER_VERSION)
+  if(GCC_COMPILER_VERSION VERSION_LESS 4.1)
+    message(ERROR "GCC 4.1 or later required, found ${GCC_COMPILER_VERSION}")
+  endif()
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Winvalid-pch -Wnon-virtual-dtor -Wsign-promo -Wshadow -Winit-self -Wno-unknown-pragmas -Wno-unused-parameter -Wno-write-strings")
   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wuninitialized")
-  if(NOT WIN32 AND NOT XCODE_VERSION)
+  if(NOT WIN32 AND NOT XCODE_VERSION AND NOT RELEASE_VERSION)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
   endif()
 endif(CMAKE_COMPILER_IS_GNUCXX)
@@ -49,15 +54,34 @@ if(MSVC)
     /wd4244 # conversion from X to Y, possible loss of data
     /wd4800 # forcing value to bool 'true' or 'false' (performance warning)
     )
+
+  # http://www.ogre3d.org/forums/viewtopic.php?f=2&t=60015&start=0
+  if(RELEASE_VERSION)
+    set(CMAKE_CXX_FLAGS "/DWIN32 /D_WINDOWS /W3 /Zm500 /EHsc /GR")
+  else()
+    set(CMAKE_CXX_FLAGS "/DWIN32 /D_WINDOWS /W3 /Zm500 /EHsc /GR /WX")
+  endif()
 endif(MSVC)
 
-if(LINUX)
-  add_definitions(-fPIC) # 64bit Linux wants this
+if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+  set(LINUX TRUE)
+  add_definitions(-fPIC)
+
+  if(REDHAT AND CMAKE_SYSTEM_PROCESSOR MATCHES "64$")
+    set(LIB_SUFFIX 64 CACHE STRING "Library directory suffix")
+  endif()
+  set(LIBRARY_DIR lib${LIB_SUFFIX})
 endif(LINUX)
 
 if(APPLE)
-  set(CMAKE_OSX_ARCHITECTURES "i386;x86_64")
+  if(_CMAKE_OSX_MACHINE MATCHES "ppc")
+    set(OSX_ARCHITECTURES "ppc;ppc64" CACHE STRING "Build architectures")
+  else()
+    set(OSX_ARCHITECTURES "i386;x86_64" CACHE STRING "Build architectures")
+  endif()
+  set(CMAKE_OSX_ARCHITECTURES ${OSX_ARCHITECTURES})
   set(CMAKE_INCLUDE_SYSTEM_FLAG_C "-isystem ")
   set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem ")
+  message(STATUS "Building for ${CMAKE_OSX_ARCHITECTURES}")
 endif(APPLE)
 
