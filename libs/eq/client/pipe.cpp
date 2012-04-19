74288a40d95fadcd176a791037dc3db2ccc0d26f
Marwan Abdellah <abdellah.marwan@gmail.com>
2012-04-19 10:02:27 +0200

    Updating experimental branche with the automatic thread placement code.

--- libs/eq/client/pipe.cpp
+++ libs/eq/client/pipe.cpp
@@ -51,6 +51,11 @@
 #include <co/worker.h>
 #include <sstream>
 
+#ifdef EQ_USE_HWLOC
+#  include <hwloc.h>
+#  include <hwloc/gl.h>
+#endif
+
 namespace eq
 {
 /** @cond IGNORE */
@@ -209,32 +214,74 @@ void Pipe::_setupCommandQueue()
     Global::leaveCarbon();
 }
 
-int Pipe::_getXDisplayPort()
+int Pipe::_getAutoAffinity()
 {
+    /* Return -1 if no port and devices were specified by the
+     * configuration files*/
+    int cpuIndex = -1;
+
     const uint32_t port = getPort();
-
-    if( !getenv( "DISPLAY" ))
-            return -1;
-
-    /* If defined port */
-    if ( port != EQ_UNDEFINED_UINT32 )
-        return static_cast<int> (port);
-    else
-        return 0;
-}
-
-int Pipe::_getXDisplayDevice()
-{
     const uint32_t device = getDevice();
 
-    if( !getenv( "DISPLAY" ))
-        return -1;
-
-    /* If defined device */
-    if ( device != EQ_UNDEFINED_UINT32 )
-        return static_cast<int> (device);
+    if (port == EQ_UNDEFINED_UINT32 || device == EQ_UNDEFINED_UINT32)
+    {
+        EQWARN << "No valid display is provided in the configuration file"
+               << std::endl;
+        EQINFO << "Automatic pipe thread placement failed" << std::endl;
+        return cpuIndex;
+    }
     else
-        return 0;
+    {
+#ifdef EQ_USE_HWLOC
+        hwloc_topology_t topology;
+        hwloc_topology_init( &topology );
+
+        /* Flags used for loading the I/O devices,
+         * bridges and their relevant info */
+        const unsigned long loading_flags = 0x0000000000000000
+                                            ^ HWLOC_TOPOLOGY_FLAG_IO_BRIDGES
+                                            ^ HWLOC_TOPOLOGY_FLAG_IO_DEVICES;
+        /* Set discovery flags */
+        const int sucess = hwloc_topology_set_flags( topology, loading_flags );
+
+        /* Flags not set */
+        if ( sucess < 0 )
+        {
+              EQINFO << "hwloc_topology_set_flags() failed," <<
+               " PCI devices will not be loaded in the topology" << std::endl;
+              EQINFO << "Automatic pipe thread placement failed" << std::endl;
+
+              return;
+        }
+        hwloc_topology_load( topology );
+
+        /* Get the cpuset for the socket connected to GPU
+        attached to the display defined by its port and device */
+        hwloc_bitmap_t cpuSet = get_display_cpuset
+            ( topology, static_cast<int> port, static_cast<int> device );
+
+        hwloc_topology_destroy(topology);
+
+        const int numCpus = hwloc_get_nbobjs_by_type
+                                          ( topology, HWLOC_OBJ_SOCKET );
+
+        for (int i = 0; i < num_cpus - 1; i++)
+        {
+            hwloc_obj_t cpuObj = hwloc_get_obj_inside_cpuset_by_type
+                                 ( topology, cpuSet, HWLOC_OBJ_SOCKET, i);
+            if (cpu_obj != 0)
+            {
+                cpuIndex = cpuObj->logical_index;
+                break;
+            }
+        }
+#else
+    EQINFO << "Missing hwloc," <<
+              "Automatic thread placement is not supported" << std::endl;
+#endif
+    }
+
+    return cpuIndex;
 }
 
 void Pipe::_setupAffinity()
@@ -247,13 +294,12 @@ void Pipe::_setupAffinity()
 
         case AUTO:
         {
-            const int port = _getXDisplayPort();
-            const int device = _getXDisplayDevice();
-
-            if ( port > -1 && device > -1 )
-                Pipe::Thread::setAutoAffinity( port, device );
+            const int autoAffinitySocket = _getAutoAffinity();
+            if ( autoAffinitySocket == -1 )
+                EQINFO << " Auto thread placement is not supported " << std::endl;
             else
-                EQWARN << "No valid display is provided " << std::endl;
+                Pipe::Thread::setAffinity
+                           ( autoAffinitySocket +  lunchbox::Thread::SOCKET );
         }
             break;
 
