cb5b1cb204636b4343936cb9528eb475e08c46a9
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-06-29 00:13:32 +0200

    Update copyright & license

--- libs/co/dataIStreamArchive.h
+++ libs/co/dataIStreamArchive.h
@@ -16,8 +16,13 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-// Based on portable_iarchive.hpp from christian.pfligersdorffer@eos.info
+// Based on portable_iarchive.hpp
 // https://github.com/boost-vault/serialization/eos_portable_archive.zip
+// Copyright Christian Pfligersdorffer, 2007. All rights reserved.
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef CO_DATAISTREAMARCHIVE_H
 #define CO_DATAISTREAMARCHIVE_H
