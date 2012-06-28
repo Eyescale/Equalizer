82e3c4c742b6f705c15abd700509048b4f3c1453
Stefan Eilemann <eilemann@gmail.com>
2012-06-28 13:12:46 +0200

    Rename seqPly application files to match class name

--- examples/seqPly/renderer.h
+++ examples/seqPly/renderer.h
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2011, Stefan Eilemann <eile@eyescale.ch> 
+/* Copyright (c) 2011-2012, Stefan Eilemann <eile@eyescale.ch> 
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -29,7 +29,7 @@
 #ifndef SEQ_PLY_RENDERER_H
 #define SEQ_PLY_RENDERER_H
 
-#include "seqPly.h"
+#include "application.h"
 #include <seq/sequel.h>
 
 namespace seqPly
