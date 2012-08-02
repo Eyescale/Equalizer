631229b394e41223ce588e8609a1e13fa57b72ad
Stefan Eilemann <eilemann@gmail.com>
2012-08-02 10:16:14 +0200

    Simplify FindMAGELLAN (#150)

--- CMake/FindMAGELLAN.cmake
+++ CMake/FindMAGELLAN.cmake
@@ -1,36 +1,23 @@
 # Copyright (c) 2011 Stefan Eilemann <eile@eyescale.ch>
 
-if(EQ_AGL_USED)
-  find_path(MAGELLAN_INCLUDE_DIR 3DconnexionClient/ConnexionClientAPI.h
-    PATHS /usr/include /usr/local/include /opt/local/include)
-  find_library(MAGELLAN_LIBRARY NAMES 3DconnexionClient)
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(MAGELLAN DEFAULT_MSG
-    MAGELLAN_INCLUDE_DIR MAGELLAN_LIBRARY)
-  if(MAGELLAN_FOUND)
-    message(STATUS "Found SpaceMouse API in ${MAGELLAN_INCLUDE_DIR};${MAGELLAN_LIBRARY}")
-  endif()
-endif()
-
-if(EQ_GLX_USED)
-  find_path(MAGELLAN_INCLUDE_DIR spnav.h
-    PATHS /usr/include /usr/local/include)
-  find_library(MAGELLAN_LIBRARY spnav
-    PATHS /usr/lib /usr/local/lib)
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(MAGELLAN DEFAULT_MSG
-    MAGELLAN_INCLUDE_DIR MAGELLAN_LIBRARY)
-  if(MAGELLAN_INCLUDE_DIR)
-    set(MAGELLAN_FOUND 1)
-    message(STATUS "Found Magellan include dir in ${MAGELLAN_INCLUDE_DIR}")
-  endif()
-  if(MAGELLAN_FOUND)
-    message(STATUS "Found SpaceMouse API in ${MAGELLAN_INCLUDE_DIR};${MAGELLAN_LIBRARY}")
-  else()
-     message("Could not find Magellan")
-  endif()
-endif()
+include(FindPackageHandleStandardArgs)
 
 if(MSVC)
   set(MAGELLAN_FOUND 1) # always on
+else()
+  if(EQ_AGL_USED)
+    find_path(MAGELLAN_INCLUDE_DIR 3DconnexionClient/ConnexionClientAPI.h
+      PATHS /usr/include /usr/local/include /opt/local/include)
+    find_library(MAGELLAN_LIBRARY NAMES 3DconnexionClient)
+  elseif(EQ_GLX_USED)
+    find_path(MAGELLAN_INCLUDE_DIR spnav.h PATHS /usr/include /usr/local/include)
+    find_library(MAGELLAN_LIBRARY spnav PATHS /usr/lib /usr/local/lib)
+  endif()
+
+  find_package_handle_standard_args(MAGELLAN DEFAULT_MSG MAGELLAN_INCLUDE_DIR
+    MAGELLAN_LIBRARY)
+  if(MAGELLAN_FOUND)
+    message(STATUS
+      "Found SpaceMouse API in ${MAGELLAN_INCLUDE_DIR};${MAGELLAN_LIBRARY}")
+  endif()
 endif()
