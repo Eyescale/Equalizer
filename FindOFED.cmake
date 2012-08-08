46df2b8cbbeb5f0e3f941e9e14b0904200138056
Stefan Eilemann <eilemann@gmail.com>
2012-08-08 10:35:31 +0200

    Implement QUIET for FindOFED

--- FindOFED.cmake
+++ FindOFED.cmake
@@ -11,13 +11,13 @@ find_library(_ibverbs_LIBRARY NAMES ibverbs
   PATHS /usr/lib /usr/local/lib /opt/local/lib)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(OFED DEFAULT_MSG
+find_package_handle_standard_args(OFED DEFAULT_MSG
   _ofed_INCLUDE_DIR _rdma_LIBRARY _ibverbs_LIBRARY)
 
 set(OFED_INCLUDE_DIRS ${_ofed_INCLUDE_DIR})
 set(OFED_LIBRARIES ${_rdma_LIBRARY})
 list(APPEND OFED_LIBRARIES ${_ibverbs_LIBRARY})
 
-if(OFED_FOUND)
+if(OFED_FOUND AND NOT OFED_FIND_QUIETLY)
   message(STATUS "Found OFED in ${OFED_INCLUDE_DIRS};${OFED_LIBRARIES}")
 endif()
