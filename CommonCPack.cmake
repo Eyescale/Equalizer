dd658326cad9a54f778afa23dbe929d64c392270
Stefan Eilemann <eilemann@gmail.com>
2012-08-15 14:44:39 +0200

    Refactor out LSBInfo gathering

--- CommonCPack.cmake
+++ CommonCPack.cmake
@@ -53,24 +53,8 @@ if(NOT CPACK_COMPONENTS_ALL)
   set(CPACK_COMPONENT_APPS_DEPENDS lib)
 endif()
 
-# LSB system information
-set(LSB_DISTRIBUTOR_ID "unknown")
-set(LSB_RELEASE "unknown")
-set(LSB_CODENAME "unknown")
-find_program(LSB_RELEASE_EXECUTABLE lsb_release)
-if(LSB_RELEASE_EXECUTABLE)
-  execute_process(COMMAND ${LSB_RELEASE_EXECUTABLE} -sc
-    OUTPUT_VARIABLE LSB_CODENAME OUTPUT_STRIP_TRAILING_WHITESPACE)
+include(LSBInfo)
 
-  execute_process(COMMAND ${LSB_RELEASE_EXECUTABLE} -sr
-    OUTPUT_VARIABLE LSB_RELEASE OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-  execute_process(COMMAND ${LSB_RELEASE_EXECUTABLE} -si
-    OUTPUT_VARIABLE LSB_DISTRIBUTOR_ID OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-  message(STATUS "LSB-Release system information: Distributor-ID: "
-    "${LSB_DISTRIBUTOR_ID}  Release: ${LSB_RELEASE}  Codename: ${LSB_CODENAME}")
-endif()
 if(CMAKE_SYSTEM_NAME MATCHES "Linux")
   find_program(RPM_EXE rpmbuild)
   find_program(DEB_EXE debuild)
