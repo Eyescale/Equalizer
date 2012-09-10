5fb454b6c89461cdf49690c12491c74acdbb5276
Stefan Eilemann <eilemann@gmail.com>
2012-09-10 08:05:09 +0200

    Fix link order of GL on Darwin

--- CMake/configure.cmake
+++ CMake/configure.cmake
@@ -9,6 +9,8 @@ if(APPLE)
   # WAR otherwise MacPorts X11 (/opt/local) is preferred
   LIST(REMOVE_ITEM CMAKE_SYSTEM_LIBRARY_PATH /opt/local/lib)
   LIST(REMOVE_ITEM CMAKE_SYSTEM_PREFIX_PATH /opt/local)
+  link_directories(/usr/X11R6/lib)
+  include_directories(SYSTEM /usr/X11R6/include)
 endif()
 
 include(FindPackages)
