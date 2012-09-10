91b2c98533b447ea36dc92d57c8a9592ff40d637
Daniel Nachbaur <danielnachbaur@gmail.com>
2012-09-10 23:42:24 +0200

    Fix non async PBO build path

--- libs/eq/client/compressor/compressorReadDrawPixels.cpp
+++ libs/eq/client/compressor/compressorReadDrawPixels.cpp
@@ -19,12 +19,16 @@
 
 #include "compressorReadDrawPixels.h"
 
+#define EQ_ASYNC_PBO // remove to use textures for async RB instead of PBOs
+
+#ifndef EQ_ASYNC_PBO
+#  include <eq/fabric/pixelViewport.h>
+#endif
 #include <eq/util/texture.h>
 #include <eq/util/pixelBufferObject.h>
 #include <lunchbox/buffer.h>
 
 #define glewGetContext() glewContext
-#define EQ_ASYNC_PBO // remove to use textures for async RB instead of PBOs
 
 namespace eq
 {
