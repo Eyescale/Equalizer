4a2bc84bf74e087b6695f97f629582be0cf6fed5
Marwan <abdellah.marwan@gmail.com>
2013-02-05 14:27:52 +0100

    Fixing a bug in the MayaFinders and also removing the manual settings of _FOUND and using FindPackageHandleStandardArgs instead. 

--- FindMaya.cmake
+++ FindMaya.cmake
@@ -9,9 +9,6 @@
 #
 ############################################################
 
-# Initially NOT found
-SET(MAYA_FOUND "")
-
 # Checking REQUIRED or NOT
 if(MAYA_FIND_REQUIRED)
   set(_maya_output_type FATAL_ERROR)
@@ -48,12 +45,6 @@ IF(MAYA_ROOT_DIR)
   IF(${_maya_output})
         MESSAGE(STATUS "MAYA_ROOT_DIR is found in : ${MAYA_ROOT_DIR}")
   ENDIF()
-  SET(MAYA_FOUND "TRUE")
-ELSE(MAYA_ROOT_DIR)
-  IF(${_maya_output})
-    MESSAGE(${_maya_output_type} "MAYA_ROOT_DIR is NOT found")
-  ENDIF()
-  SET(MAYA_FOUND "")
 
   # Include & libraries directories
   SET(MAYA_INCLUDE_DIR "${MAYA_ROOT_DIR}/include")
@@ -115,30 +106,27 @@ ELSE(MAYA_ROOT_DIR)
    )
 
   # Adding libraries to the MAYA_LIBRARY list to be linking against
-  SET(MAYA_BASE_LIBRARIES   ${MAYA_Foundation_LIBRARY}
-                            ${MAYA_OpenMaya_LIBRARY})
-  SET(MAYA_ANIM_LIBRARIES   ${MAYA_BASE_LIBRARIES}
-                            ${MAYA_OpenMayaAnim_LIBRARY} )
-  SET(MAYA_UI_LIBRARIES     ${MAYA_BASE_LIBRARIES}
-                            ${MAYA_OpenMayaUI_LIBRARY})
-  SET(MAYA_RENDER_LIBRARIES ${MAYA_BASE_LIBRARIES}
-                            ${MAYA_OpenMayaRender_LIBRARY})
-  SET(MAYA_FX_LIBRARIES     ${MAYA_BASE_LIBRARIES}
-                            ${MAYA_OpenMayaFX_LIBRARY})
-  SET(MAYA_CLOTH_LIBRARIES  ${MAYA_BASE_LIBRARIES}
-                            ${MAYA_Cloth_LIBRARY})
-  SET(MAYA_IMAGE_LIBRARIES  ${MAYA_Image_LIBRARY})
-
-  SET(MAYA_LIBRARIES 		    ${MAYA_BASE_LIBRARIES}
-                            ${MAYA_ANIM_LIBRARIES}
-                            ${MAYA_UI_LIBRARIES}
-                            ${MAYA_FX_LIBRARIES}
-                            ${MAYA_IMAGE_LIBRARIES})
+  SET(MAYA_LIBRARIES ${MAYA_Foundation_LIBRARY}
+                     ${MAYA_OpenMaya_LIBRARY}
+                     ${MAYA_OpenMayaAnim_LIBRARY}
+                     ${MAYA_OpenMayaUI_LIBRARY} 
+                     ${MAYA_OpenMayaRender_LIBRARY}
+                     ${MAYA_OpenMayaFX_LIBRARY}
+                     ${MAYA_Cloth_LIBRARY}
+                     ${MAYA_Image_LIBRARY})
 
   # Adding BASIC definitions
   ADD_DEFINITIONS(-D_BOOL -DFUNCPROTO -DREQUIRE_IOSTREAM)
-ENDIF(MAYA_ROOT_DIR)
 
+  INCLUDE(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(MAYA DEFAULT_MSG MAYA_ROOT_DIR 
+	MAYA_LIBRARIES)
+
+ELSE(MAYA_ROOT_DIR)
+  IF(${_maya_output})
+    MESSAGE(${_maya_output_type} "MAYA_ROOT_DIR is NOT found")
+  ENDIF()
+ENDIF(MAYA_ROOT_DIR)
 
 IF(MAYA_FOUND AND _maya_output)
   MESSAGE(STATUS "Found MAYA in ${MAYA_INCLUDE_DIR} ${MAYA_LIBRARIES}")
