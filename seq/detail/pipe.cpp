a8a51a2b0e06c561973dbfa26b6c5e23602ae60c
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2013-03-21 10:42:09 +0100

    Fix seqPly exit

--- seq/detail/pipe.cpp
+++ seq/detail/pipe.cpp
@@ -1,15 +1,15 @@
 
-/* Copyright (c) 2011-2012, Stefan Eilemann <eile@eyescale.ch> 
+/* Copyright (c) 2011-2012, Stefan Eilemann <eile@eyescale.ch>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
  * by the Free Software Foundation.
- *  
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -32,7 +32,7 @@ namespace detail
 {
 
 Pipe::Pipe( eq::Node* parent )
-        : eq::Pipe( parent ) 
+        : eq::Pipe( parent )
         , _objects( 0 )
         , _renderer( 0 )
 {}
@@ -140,6 +140,7 @@ void Pipe::_unmapData()
 {
     LBASSERT( _objects )
         getConfig()->unmapObject( _objects );
+    _objects->clear();
     delete _objects;
     _objects = 0;
 }
