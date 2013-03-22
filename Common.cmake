905ebda808cd18086f9450f4258f8fe09b8283af
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-03-22 14:13:35 +0100

    Boost/CMake fix

--- Common.cmake
+++ Common.cmake
@@ -68,6 +68,7 @@ endif()
 if(BOOST_ROOT)
   set(Boost_NO_SYSTEM_PATHS TRUE)
 endif()
+set(Boost_NO_BOOST_CMAKE ON CACHE BOOL "Enable fix for FindBoost.cmake" )
 add_definitions(-DBOOST_ALL_NO_LIB) # Don't use 'pragma lib' on Windows
 
 include(TestBigEndian)
