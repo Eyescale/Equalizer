e20796b31bf510bde807039aaeb5bade14ca72d1
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-05-15 15:21:44 +0200

    Generalize Spacemouse Finder

--- FindMAGELLAN.cmake
+++ FindMAGELLAN.cmake
@@ -1,17 +1,38 @@
-# Copyright (c) 2011 Stefan Eilemann <eile@eyescale.ch>
+# Copyright (c) 2011-2013 Stefan Eilemann <eile@eyescale.ch>
 
-if(EQ_AGL_USED)
-  find_path(MAGELLAN_INCLUDE_DIR 3DconnexionClient/ConnexionClientAPI.h
-    PATHS /usr/include /usr/local/include /opt/local/include)
-  find_library(MAGELLAN_LIBRARY NAMES 3DconnexionClient)
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(MAGELLAN DEFAULT_MSG
-    MAGELLAN_INCLUDE_DIR MAGELLAN_LIBRARY)
-  if(MAGELLAN_FOUND)
-    message(STATUS "Found SpaceMouse API in ${MAGELLAN_INCLUDE_DIR};${MAGELLAN_LIBRARY}")
-  endif()
+include(FindPackageHandleStandardArgs)
+
+set(MAGELLAN_API "spnav")
+if(APPLE)
+  set(MAGELLAN_API "Carbon")
+endif()
+if(MSVC)
+  set(MAGELLAN_API "Windows")
+endif()
+if(MAGELLAN_FIND_COMPONENTS)
+  set(MAGELLAN_API ${MAGELLAN_FIND_COMPONENTS})
 endif()
 
-if(MSVC)
+if(MAGELLAN_API STREQUAL "Windows")
   set(MAGELLAN_FOUND 1) # always on
+  return()
+endif()
+
+if(MAGELLAN_API STREQUAL "Carbon")
+  find_path(MAGELLAN_INCLUDE_DIR 3DconnexionClient/ConnexionClientAPI.h
+    PATHS /usr/include /usr/local/include /opt/local/include)
+  set(MAGELLAN_LIBRARY)
+endif()
+
+if(MAGELLAN_API STREQUAL "spnav")
+  find_path(MAGELLAN_INCLUDE_DIR spnav.h PATHS /usr/include /usr/local/include)
+  find_library(MAGELLAN_LIBRARY spnav PATHS /usr/lib /usr/local/lib)
+endif()
+
+find_package_handle_standard_args(MAGELLAN DEFAULT_MSG MAGELLAN_INCLUDE_DIR
+  MAGELLAN_LIBRARY)
+
+if(MAGELLAN_FOUND AND NOT MAGELLAN_FIND_QUIETLY)
+  message(STATUS
+    "Found SpaceMouse API in ${MAGELLAN_INCLUDE_DIR};${MAGELLAN_LIBRARY}")
 endif()
