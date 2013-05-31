2a17a283b4c29850743491d070e2c1d43452bb19
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-05-31 14:44:05 +0200

    Add glxewContextInit needed on OS X since MacPorts GLEXMx does not cover GLX

--- FindGLEW_MX.cmake
+++ FindGLEW_MX.cmake
@@ -1,5 +1,5 @@
 # Copyright (c) 2010 Daniel Pfeifer <daniel@pfeifer-mail.de>
-#               2011 Stefan Eilemann <eile@eyescale.ch>
+#               2011-2013 Stefan Eilemann <eile@eyescale.ch>
 
 find_path(_glew_mx_INCLUDE_DIR GL/glew.h
   /usr/include /usr/local/include /opt/local/include)
@@ -15,16 +15,13 @@ if(_glew_mx_INCLUDE_DIR AND _glew_mx_LIBRARY)
     "int main(int argc, char* argv[])\n"
     "{\n"
     "  glewContextInit(0);\n"
+    "}\n"
     )
-  if(X11_FOUND)
-    file(APPEND ${TEST_SRC} "  glxewContextInit();\n")
-  endif()
-  file(APPEND ${TEST_SRC} "}\n")
 
   try_compile(_glew_mx_SUPPORTED ${CMAKE_BINARY_DIR}/glew_test ${TEST_SRC}
     CMAKE_FLAGS
-      "-DINCLUDE_DIRECTORIES:STRING=${_glew_mx_INCLUDE_DIR}"
-      "-DLINK_LIBRARIES:STRING=${_glew_mx_LIBRARY}"
+    "-DINCLUDE_DIRECTORIES:STRING=${_glew_mx_INCLUDE_DIR}"
+    "-DLINK_LIBRARIES:STRING=${_glew_mx_LIBRARY}"
     COMPILE_DEFINITIONS -DGLEW_MX=1
     )
 
@@ -32,7 +29,27 @@ if(_glew_mx_INCLUDE_DIR AND _glew_mx_LIBRARY)
     message(STATUS "  ${_glew_mx_LIBRARY} does not support GLEW_MX.")
     set(_glew_mx_INCLUDE_DIR 0)
     set(_glew_mx_LIBRARY 0)
-  endif(NOT _glew_mx_SUPPORTED)
+  elseif(X11_FOUND)
+    file(WRITE ${TEST_SRC}
+      "#include <GL/glxew.h>\n"
+      "int main(int argc, char* argv[])\n"
+      "{\n"
+      "  glxewContextInit(0);\n"
+      "}\n"
+      )
+
+    try_compile(_glxew_mx_SUPPORTED ${CMAKE_BINARY_DIR}/glew_test ${TEST_SRC}
+      CMAKE_FLAGS
+      "-DINCLUDE_DIRECTORIES:STRING=${_glew_mx_INCLUDE_DIR}"
+      "-DLINK_LIBRARIES:STRING=${_glew_mx_LIBRARY}"
+      COMPILE_DEFINITIONS -DGLEW_MX=1
+      )
+    if(NOT _glxew_mx_SUPPORTED)
+      message(STATUS "  ${_glew_mx_LIBRARY} is missing glxewContextInit().")
+      set(_glew_mx_INCLUDE_DIR 0)
+      set(_glew_mx_LIBRARY 0)
+    endif()
+  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
