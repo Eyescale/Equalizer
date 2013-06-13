ba5d956603779ddcb9d355fac135d20c84916781
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-06-13 14:33:32 +0200

    Fix #76: 7-window.DPLEX.2D.lb.eqc does not load-balance by doing a pre-equalizer compound update for activation and a post-equalizer update to bring equalizer changes into effect

--- eq/server/compoundInitVisitor.cpp
+++ eq/server/compoundInitVisitor.cpp
@@ -1,17 +1,17 @@
 
-/* Copyright (c) 2007-2012, Stefan Eilemann <eile@equalizergraphics.com>
+/* Copyright (c) 2007-2013, Stefan Eilemann <eile@equalizergraphics.com>
  *                    2011, Daniel Nachbaur <danielnachbaur@gmail.com>
  *                    2010, Cedric Stalder <cedric.stalder@gmail.com>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
  * by the Free Software Foundation.
- *  
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -51,7 +51,7 @@ VisitorResult CompoundInitVisitor::visit( Compound* compound )
         ( compound->isDestination() && !channel->getSegment( )))
     {
         // Note: The second case are non-view destination compounds. One use
-        // case are swap-syncing all output channels using task-less compounds.
+        // case is swap-syncing all output channels using task-less compounds.
 
         LBASSERT( !channel || !channel->getView( ));
         uint32_t eyes = compound->getEyes();
@@ -66,7 +66,7 @@ VisitorResult CompoundInitVisitor::visit( Compound* compound )
         compound->updateInheritTasks();
         channel->addTasks( compound->getInheritTasks( ));
     }
-    return TRAVERSE_CONTINUE;    
+    return TRAVERSE_CONTINUE;
 }
 
 }
