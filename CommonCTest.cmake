a1d07faad8e08285c6e2f695b2241cf64f6a33e1
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2013-11-08 15:28:13 +0100

    Check for incompatible lcov/gcc combinations

--- CommonCTest.cmake
+++ CommonCTest.cmake
@@ -5,33 +5,49 @@ if(NOT WIN32) # tests want to be with DLLs on Windows - no rpath
   set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tests)
 endif()
 
-find_program(LCOV lcov)
-find_program(GENHTML genhtml)
-if(LCOV AND GENHTML AND (CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG))
-  set(COVERAGE ON)
-  add_custom_target(lcov-clean
-    COMMAND ${LCOV} -q --directory ${CMAKE_BINARY_DIR} --zerocounters
-    COMMENT "Resetting code coverage counters")
-else()
-  set(COVERAGE_MISSING)
-  if(NOT LCOV)
-    set(COVERAGE_MISSING " lcov")
+if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANG)
+  find_program(GENHTML genhtml)
+  find_program(LCOV lcov)
+  if(LCOV)
+    execute_process(COMMAND
+      ${LCOV} --version OUTPUT_VARIABLE LCOV_VERSION
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string(REGEX REPLACE ".+([0-9]+\\.[0-9]+)" "\\1" LCOV_VERSION
+      ${LCOV_VERSION})
   endif()
-  if(NOT GENHTML)
-    set(COVERAGE_MISSING "${COVERAGE_MISSING} genhtml")
+  if( GCC_COMPILER_VERSION VERSION_GREATER 4.6.99 AND
+      LCOV_VERSION VERSION_LESS 1.10)
+    message(STATUS "Need lcov >= 1.10 for gcc ${GCC_COMPILER_VERSION}, found lcov ${LCOV_VERSION}")
+    set(LCOV)
   endif()
-  if(COVERAGE_MISSING)
-    set(COVERAGE_MISSING "missing${COVERAGE_MISSING}")
+
+  if(LCOV AND GENHTML)
+    set(COVERAGE ON)
+    add_custom_target(lcov-clean
+      COMMAND ${LCOV} -q --directory ${CMAKE_BINARY_DIR} --zerocounters
+      COMMENT "Resetting code coverage counters")
   else()
-    set(COVERAGE_MISSING "unsupported compiler")
+    set(COVERAGE_MISSING)
+    if(NOT LCOV)
+      set(COVERAGE_MISSING " lcov")
+    endif()
+    if(NOT GENHTML)
+      set(COVERAGE_MISSING "${COVERAGE_MISSING} genhtml")
+    endif()
+    if(COVERAGE_MISSING)
+      set(COVERAGE_MISSING "missing${COVERAGE_MISSING}")
+    else()
+      set(COVERAGE_MISSING "unsupported compiler")
+    endif()
+    if(LSB_DISTRIBUTOR_ID STREQUAL "Ubuntu")
+      set(COVERAGE_MISSING "")
+    endif()
+
+    message(STATUS "No code coverage report, ${COVERAGE_MISSING}")
   endif()
-  message(STATUS "No code coverage report, ${COVERAGE_MISSING}")
 endif()
 
-include_directories(
-  ${CMAKE_CURRENT_LIST_DIR}/test
-  ${CMAKE_CURRENT_SOURCE_DIR}
-  )
+include_directories(${CMAKE_CURRENT_LIST_DIR}/test ${CMAKE_CURRENT_SOURCE_DIR})
 
 file(GLOB_RECURSE TEST_FILES RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} *.cpp)
 list(SORT TEST_FILES)
