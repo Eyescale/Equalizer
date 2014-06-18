627dad9b3843c4482e0c7e2e50c084861b54f5f7
Ahmet Bilgili <ahmet.bilgili@epfl.ch>
2014-06-18 10:30:08 +0200

    Release notes, CMakeLists.txt updated for release 1.7

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -11,13 +11,13 @@ set(CPACK_RESOURCE_FILE_README ${CPACK_PACKAGE_DESCRIPTION_FILE})
 
 set(EQ_IB_PACKAGES "librdmacm-dev, libibverbs-dev, librdmacm-dev")
 set(CPACK_DEBIAN_BUILD_DEPENDS bison flex libboost-system-dev
-  libboost-date-time-dev libboost-regex-dev libboost-serialization-dev
-  libx11-dev libgl1-mesa-dev libglewmx1.6-dev libspnav0
-  librdmacm-dev libibverbs-dev librdmacm-dev
+  libboost-filesystem-dev libboost-date-time-dev libboost-regex-dev libboost-serialization-dev
+  libboost-program-options-dev libx11-dev libgl1-mesa-dev libglewmx1.6-dev 
+  libspnav0 librdmacm-dev libibverbs-dev librdmacm-dev
   ${COLLAGE_DEB_DEV_DEPENDENCY} ${HWSD_DEB_DEV_DEPENDENCY}
   ${GLSTATS_DEB_DEV_DEPENDENCY} ${LUNCHBOX_DEB_DEV_DEPENDENCY}
   ${VMMLIB_DEB_DEPENDENCIES})
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6, libboost-system-dev, libboost-date-time-dev, libboost-regex-dev, libboost-serialization-dev, libx11-dev, libgl1-mesa-dev, libglewmx1.6-dev, libspnav-dev, ${EQ_IB_PACKAGES}, ${COLLAGE_DEB_DEV_DEPENDENCY}, ${HWSD_DEB_DEPENDENCIES}, ${GLSTATS_DEB_DEV_DEPENDENCY}, ${LUNCHBOX_DEB_DEV_DEPENDENCY}, ${VMMLIB_DEB_DEPENDENCIES}")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libstdc++6, libboost-system-dev, libboost-date-time-dev, libboost-regex-dev, libboost-program-options-dev, libboost-filesystem-dev, libboost-serialization-dev, libx11-dev, libgl1-mesa-dev, libglewmx1.6-dev, libspnav-dev, ${EQ_IB_PACKAGES}, ${COLLAGE_DEB_DEV_DEPENDENCY}, ${HWSD_DEB_DEPENDENCIES}, ${GLSTATS_DEB_DEV_DEPENDENCY}, ${LUNCHBOX_DEB_DEV_DEPENDENCY}, ${VMMLIB_DEB_DEPENDENCIES}")
 
 set(CPACK_MACPORTS_CATEGORY graphics)
 set(CPACK_MACPORTS_DEPENDS boost hwsd GLStats Collage Lunchbox VMMLIB)
@@ -29,3 +29,4 @@ string(REPLACE "GLEW_MX" "glewmx" CPACK_PACKAGE_CONFIG_REQUIRES
 
 set(UBUNTU_LP_BUG 300472)
 include(CommonCPack)
+include(OSSCPack)
