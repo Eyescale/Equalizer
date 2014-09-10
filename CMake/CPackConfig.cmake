e124a5e897140283e617f29bb2ab62a65d941653
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-09-10 14:48:19 +0200

    Fixes to make Eq sub-project capable

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -6,13 +6,13 @@
 set(CPACK_PACKAGE_VENDOR "www.eyescale.ch")
 set(CPACK_PACKAGE_CONTACT "Stefan Eilemann <eile@eyescale.ch>")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Parallel Rendering Framework")
-set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_SOURCE_DIR}/doc/RelNotes.md)
+set(CPACK_PACKAGE_DESCRIPTION_FILE ${PROJECT_SOURCE_DIR}/doc/RelNotes.md)
 set(CPACK_RESOURCE_FILE_README ${CPACK_PACKAGE_DESCRIPTION_FILE})
 
 set(EQ_IB_PACKAGES "librdmacm-dev, libibverbs-dev, librdmacm-dev")
 set(CPACK_DEBIAN_BUILD_DEPENDS bison flex libboost-system-dev
   libboost-filesystem-dev libboost-date-time-dev libboost-regex-dev libboost-serialization-dev
-  libboost-program-options-dev libx11-dev libgl1-mesa-dev libglewmx1.6-dev 
+  libboost-program-options-dev libx11-dev libgl1-mesa-dev libglewmx1.6-dev
   libspnav0 librdmacm-dev libibverbs-dev librdmacm-dev
   ${COLLAGE_DEB_DEV_DEPENDENCY} ${HWSD_DEB_DEV_DEPENDENCY}
   ${GLSTATS_DEB_DEV_DEPENDENCY} ${LUNCHBOX_DEB_DEV_DEPENDENCY}
