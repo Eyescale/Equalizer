9e27823dcdcd422b9f7c5f81989bbf2698f156c5
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-09-29 13:17:09 +0200

    AABBf serialization support

--- eq/fabric/vmmlib.h
+++ eq/fabric/vmmlib.h
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2011-2012, Stefan Eilemann <eile@eyescale.ch>
+/* Copyright (c) 2011-2014, Stefan Eilemann <eile@eyescale.ch>
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 2.1 as published
@@ -35,21 +35,22 @@ namespace eq
 {
 namespace fabric
 {
-typedef vmml::matrix< 3, 3, double > Matrix3d; //!< A 3x3 double matrix
-typedef vmml::matrix< 4, 4, double > Matrix4d; //!< A 4x4 double matrix
-typedef vmml::matrix< 3, 3, float >  Matrix3f; //!< A 3x3 float matrix
-typedef vmml::matrix< 4, 4, float >  Matrix4f; //!< A 4x4 float matrix
-typedef vmml::vector< 2, int > Vector2i; //!< A two-component integer vector
-typedef vmml::vector< 3, int > Vector3i; //!< A three-component integer vector
-typedef vmml::vector< 4, int > Vector4i; //!< A four-component integer vector
-typedef vmml::vector< 3, double >Vector3d; //!< A three-component double vector
-typedef vmml::vector< 4, double >Vector4d; //!< A four-component double vector
-typedef vmml::vector< 2, float > Vector2f; //!< A two-component float vector
-typedef vmml::vector< 3, float > Vector3f; //!< A three-component float vector
-typedef vmml::vector< 4, float > Vector4f; //!< A four-component float vector
-typedef vmml::vector< 3, uint8_t > Vector3ub; //!< A three-component byte vector
-typedef vmml::vector< 4, uint8_t > Vector4ub; //!< A four-component byte vector
-typedef vmml::frustum< float >  Frustumf; //!< A frustum definition
+using vmml::Matrix3d; //!< A 3x3 double matrix
+using vmml::Matrix4d; //!< A 4x4 double matrix
+using vmml::Matrix3f; //!< A 3x3 float matrix
+using vmml::Matrix4f; //!< A 4x4 float matrix
+using vmml::Vector2i; //!< A two-component integer vector
+using vmml::Vector3i; //!< A three-component integer vector
+using vmml::Vector4i; //!< A four-component integer vector
+using vmml::Vector3d; //!< A three-component double vector
+using vmml::Vector4d; //!< A four-component double vector
+using vmml::Vector2f; //!< A two-component float vector
+using vmml::Vector3f; //!< A three-component float vector
+using vmml::Vector4f; //!< A four-component float vector
+using vmml::Vector3ub; //!< A three-component byte vector
+using vmml::Vector4ub; //!< A four-component byte vector
+using vmml::Frustumf; //!< A frustum definition
+using vmml::AABBf; //!< axis-aligned bounding box
 }
 }
 
@@ -107,6 +108,12 @@ template<> inline void byteswap( eq::fabric::Frustumf& value )
     byteswap( value.top( ));
     byteswap( value.near_plane( ));
     byteswap( value.far_plane( ));
+}
+
+template<> inline void byteswap( eq::fabric::AABBf& value )
+{
+    byteswap( value.getMin( ));
+    byteswap( value.getMax( ));
 }
 }
 
