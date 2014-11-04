4427202f4c5ea4002a20b50cca125f85bf920010
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2014-11-04 10:21:08 +0100

    Use local server directly from server library rather than dlopen it

---
+++ eq/server/localServer.h
@@ -0,0 +1,36 @@
+
+/* Copyright (c) 2014, Daniel Nachbaur <danielnachbaur@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 2.1 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef EQSERVER_LOCALSERVER_H
+#define EQSERVER_LOCALSERVER_H
+
+#include "api.h"
+#include "types.h"
+
+namespace eq
+{
+namespace server
+{
+
+EQSERVER_API co::ConnectionPtr startLocalServer( const std::string& config );
+
+EQSERVER_API void joinLocalServer();
+
+}
+}
+
+#endif // EQSERVER_LOCALSERVER_H
