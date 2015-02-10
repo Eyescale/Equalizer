bbe69812fa4077c1e70b5e294e5515c11f42942a
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-02-10 17:58:21 +0100

    EqGLLibraries: export correct GLEW_MX lib

--- CMake/EqGLLibraries.cmake
+++ CMake/EqGLLibraries.cmake
@@ -1,8 +1,13 @@
-# Copyright (c) 2014 Stefan.Eilemann@epfl.ch
-# sets EQ_GL_LIBRARIES as a function of EQ_AGL/GLX/WGL_USED
-#   find_package(OpenGL) and find_package(Equalizer) need to be done beforehand
+# Copyright (c) 2014-2015 Stefan.Eilemann@epfl.ch
+#
+# find_package(OpenGL) and find_package(Equalizer) need to be done beforehand
+#
+# Sets:
+# - EQ_GL_LIBRARIES as a function of EQ_AGL/GLX/WGL_USED
+# - GLEW_MX_LIBRARIES to the static Eq-supplied one if GLEW_MX was not found
 
 set(EQ_GL_LIBRARIES)
+
 if(APPLE)
   if(EQ_AGL_USED)
     list(APPEND EQ_GL_LIBRARIES ${OPENGL_LIBRARIES})
@@ -13,4 +18,19 @@ if(APPLE)
   endif()
 else()
   list(APPEND EQ_GL_LIBRARIES ${OPENGL_gl_LIBRARY})
+endif()
+
+if(NOT GLEW_MX_FOUND)
+  find_package(GLEW_MX QUIET)
+  if(NOT GLEW_MX_FOUND)
+    set(GLEW_MX_LIBRARIES ${GLEW_MX_EQUALIZER_LIBRARY})
+    # eile WAR: In Livre, ${GLEW_MX_EQUALIZER_LIBRARY} is lost when
+    # including this file, that is, it is set before the include but
+    # no longer in this file. I could not figure out why, so simply
+    # workaround it here:
+    if(NOT GLEW_MX_LIBRARIES)
+      set(GLEW_MX_LIBRARIES GLEW_MX_Equalizer)
+    endif()
+    add_definitions(-DGLEW_STATIC=1 -DGLEW_MX=1)
+  endif()
 endif()
